9d9edfdacca7abb1cfe2a7f110353145a976a1c8
azuki <yuhi55.k@gmail.com>
2023-06-10 18:33:47 +0900

    ポイントライト構造体とスポットライト構造体。セット関数追加

--- k.k/k2EngineLow/Light.h
+++ k.k/k2EngineLow/Light.h
@@ -3,6 +3,9 @@ namespace nsK2EngineLow {
 
 	const int MAM_DIRECTIONAL_LIGHT = 4;   //ディレクションライトの最大数
 
+	/// <summary>
+	/// ディレクションライト構造体
+	/// </summary>
 	struct DirectionalLight
 	{
 		Vector3 lightDirection;		//ライトの方向
@@ -13,15 +16,49 @@ namespace nsK2EngineLow {
 	};
 
 	/// <summary>
+	/// ポイントライト構造体
+	/// </summary>
+	struct PointLight
+	{
+		Vector3 position;		//座標
+		float pad0;				//パティング0
+		Vector3 color;			//カラー
+		float pad1;				//パティング1
+		Vector3 attn;			//xに影響範囲、yに影響率に累乗するパラメータ
+		int	isUse = false;		//使用中フラグ
+
+	};
+
+	/// <summary>
+	/// スポットライト構造体
+	/// </summary>
+	struct SpotLight
+	{
+		Vector3 position;		//座標
+		float pad0;				//パティング0
+		Vector3 angle;			//xに射出角度、yに影響に累乗するパラメータ
+		float pad1;				//パティング1
+		Vector3 color;			//カラー
+		float pad2;				//パティング2
+		Vector3 attn;			//xに影響範囲、yに影響率に累乗するパラメータ
+		int	isUse = false;		//使用中フラグ
+		Vector3 direction;		//照射方向
+		float pad3;				//パティング3
+	};
+
+	/// <summary>
 	/// ライト構造体
 	/// </summary>
 	struct Light
 	{
-		DirectionalLight directionalLight;
-		float pad0;
-		float pad1;
-		Matrix mLVP;
-		Vector3 cameraEyePos;
+		DirectionalLight	directionalLight;		//ディレクションライト
+		PointLight			pointLight;				//ポイントライト
+		SpotLight			spotLight;				//スポットライト
+		Vector3             ambientLight;			//環境光(アンビエントライト)
+		float				pad0;					//パティング０
+		float				pad1;					//パティング１
+		Matrix				mLVP;					//
+		Vector3				cameraEyePos;			//カメラの座標
 	};
 
 
@@ -88,6 +125,253 @@ namespace nsK2EngineLow {
 		const Vector3& GetDirLigColor() const
 		{
 			return m_light.directionalLight.color;
+		}
+
+		////////////////////////////////////////////////////////
+		///環境光の関数
+		////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// 環境光の設定
+		/// </summary>
+		/// <param name="ambient">環境光の強さ</param>
+		void SetAmbient(Vector3 ambient)
+		{
+			m_light.ambientLight = ambient;
+		}
+
+		////////////////////////////////////////////////////////
+		///ポイントライトの関数
+		////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// ポイントライトの設定
+		/// </summary>
+		/// <param name="pos">座標</param>
+		/// <param name="color">色</param>
+		/// <param name="attn">Xに影響範囲,Yに累乗するパラメータ</param>
+		void SetPointLight(Vector3 pos, Vector3 color, Vector3 attn)
+		{
+			SetPointLightPosition(pos);
+			SetPointLightColor(color);
+			SetPointLightAttn(attn);
+			UsePointLight();
+		}
+
+		/// <summary>
+		/// ポイントライトの座標
+		/// </summary>
+		/// <param name="pos">座標</param>
+		void SetPointLightPosition(Vector3 pos)
+		{
+			m_light.pointLight.position = pos;
+		}
+
+		/// <summary>
+		/// ポイントライトの色
+		/// </summary>
+		/// <param name="color">色</param>
+		void SetPointLightColor(Vector3 color)
+		{
+			m_light.pointLight.color = color;
+		}
+
+		/// <summary>
+		/// 影響範囲と影響率に累乗するパラメータを設定
+		/// </summary>
+		/// <param name="attn">Xに影響範囲,Yに累乗するパラメータ</param>
+		void SetPointLightAttn(Vector3 attn)
+		{
+			m_light.pointLight.attn = attn;
+		}
+
+		/// <summary>
+		/// ポイントライトを使用する
+		/// </summary>
+		void UsePointLight()
+		{
+			m_light.pointLight.isUse = true;
+		}
+
+		/// <summary>
+		/// ポイントライトを使用しない
+		/// </summary>
+		void UnUsePointLight()
+		{
+			m_light.pointLight.isUse = false;
+		}
+
+		/// <summary>
+		/// ポイントライトの位置を取得する
+		/// </summary>
+		/// <returns>座標</returns>
+		const Vector3& GetPointLightPosition() const
+		{
+			return m_light.pointLight.position;
+		}
+
+		/// <summary>
+		/// ポイントライトの光の色を取得
+		/// </summary>
+		/// <returns>色</returns>
+		const Vector3& GetPointLightColor() const
+		{
+			return m_light.pointLight.color;
+		}
+
+		/// <summary>
+		/// スポットライトの影響範囲と累乗するパラメータを取得
+		/// </summary>
+		/// <returns>Xに影響範囲,Yに累乗するパラメータ</returns>
+		const Vector3& GetPointLightAttn() const
+		{
+			return m_light.pointLight.attn;
+		}
+
+		/// <summary>
+		/// ポイントライトは使用中?
+		/// </summary>
+		/// <returns>使用中ならtrue</returns>
+		const int PointLightIsUse() const
+		{
+			return m_light.pointLight.isUse;
+		}
+
+		////////////////////////////////////////////////////////
+		///スポットライトの関数
+		////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// スポットライトを設定する
+		/// </summary>
+		/// <param name="pos">位置</param>
+		/// <param name="color">色</param>
+		/// <param name="range">Xに影響範囲,Yに累乗するパラメータ</param>
+		/// <param name="direction">方向</param>
+		/// <param name="angle">角度</param>
+		void SetSpotLight(Vector3 pos, Vector3 color, Vector3 attn, Vector3 direction, Vector3 angle)
+		{
+			SetSpotLightPosition(pos);
+			SetSpotLightColor(color);
+			SetSpotLightAttn(attn);
+			SetSpotLightDirection(direction);
+			SetSpotLightAngle(angle);
+			UseSpotLight();
+		}
+
+		/// <summary>
+		/// スポットライトの位置を設定
+		/// </summary>
+		/// <param name="pos">座標</param>
+		void SetSpotLightPosition(Vector3 pos)
+		{
+			m_light.spotLight.position = pos;
+		}
+
+		/// <summary>
+		/// スポットライトのライト色の設定
+		/// </summary>
+		/// <param name="color">色</param>
+		void SetSpotLightColor(Vector3 color)
+		{
+			m_light.spotLight.color = color;
+		}
+
+		/// <summary>
+		/// 影響範囲と累乗するパラメータを設定
+		/// </summary>
+		/// <param name="attn">Xに影響範囲,Yに累乗するパラメータ</param>
+		void SetSpotLightAttn(Vector3 attn)
+		{
+			m_light.spotLight.attn = attn;
+		}
+
+		/// <summary>
+		/// スポットライトのライトの方向を設定
+		/// </summary>
+		/// <param name="direction">方向</param>
+		void SetSpotLightDirection(Vector3 direction)
+		{
+			m_light.spotLight.direction = direction;
+		}
+
+		/// <summary>
+		/// スポットライトのライトの角度を設定
+		/// </summary>
+		/// <param name="angle">角度</param>
+		void SetSpotLightAngle(Vector3 angle)
+		{
+			m_light.spotLight.angle = angle;
+		}
+
+		/// <summary>
+		/// スポットライトを使用する
+		/// </summary>
+		void UseSpotLight()
+		{
+			m_light.spotLight.isUse = true;
+		}
+
+		/// <summary>
+		/// スポットライトを使用しない
+		/// </summary>
+		void UnUseSpotLight()
+		{
+			m_light.spotLight.isUse = false;
+		}
+
+		/// <summary>
+		/// スポットライトの位置を取得
+		/// </summary>
+		/// <returns>座標</returns>
+		const Vector3& GetSpotLightPosition() const
+		{
+			return m_light.spotLight.position;
+		}
+
+		/// <summary>
+		/// スポットライトの光の色を取得
+		/// </summary>
+		/// <returns>色</returns>
+		const Vector3& GetSpotLightColor() const
+		{
+			return m_light.spotLight.color;
+		}
+
+		/// <summary>
+		/// スポットライトの影響範囲と累乗するパラメータを取得
+		/// </summary>
+		/// <returns>Xに影響範囲,Yに累乗するパラメータ</returns>
+		const Vector3& GetSpotLightAttn()const
+		{
+			return m_light.spotLight.attn;
+		}
+
+		/// <summary>
+		/// スポットライトの光の方向を取得
+		/// </summary>
+		/// <returns>光の方向</returns>
+		const Vector3& GetSpotLightDirection() const
+		{
+			return m_light.spotLight.direction;
+		}
+
+		/// <summary>
+		/// スポットライトの角度を取得する
+		/// </summary>
+		/// <returns>角度</returns>
+		const Vector3& GetSpotLightAngle() const
+		{
+			return m_light.spotLight.angle;
+		}
+
+		/// <summary>
+		/// スポットライトは使用中？
+		/// </summary>
+		/// <returns>使用中の場合true</returns>
+		const int SpotLightIsUse() const
+		{
+			return m_light.spotLight.isUse;
 		}
 
 		////////////////////////////////////////////////////////
