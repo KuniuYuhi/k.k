fc70da44a410fea78d9c59af6d2c3b4dc0860b49
azuki <yuhi55.k@gmail.com>
2023-07-03 00:58:55 +0900

    アクタークラス追加。移動関数追加

--- k.k/Game/Actor.h
+++ k.k/Game/Actor.h
@@ -7,16 +7,32 @@ public:
 	Actor();
 	virtual ~Actor();
 
+	const Vector3 GetPosition()
+	{
+		return m_position;
+	}
+
 protected:
-	void Move();
+
+	void SetTransFormModel(ModelRender& modelRender);
+
+	virtual void InitModel()=0;
+
+	Vector3 Move(Status status);
+
+	Quaternion Rotation();
 
 
-private:
+
+protected:
 	Status m_status;
-
 
 	Vector3 m_position = Vector3::Zero;
 	Vector3 m_moveSpeed = Vector3::Zero;
+	Quaternion m_rotation = Quaternion::Identity;
+	Vector3 m_scale = Vector3::One;
+
+	bool m_dashFlag = false;		//ダッシュするかのフラグ
 
 };
 
