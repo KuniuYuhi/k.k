768738e930b61cadc4a2d60adeca2e6bdd7d190a
azuki <yuhi55.k@gmail.com>
2023-07-06 10:10:56 +0900

    コリジョンオブジェクトの確認

--- k.k/Game/BossStage1.cpp
+++ k.k/Game/BossStage1.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "BossStage1.h"
+#include "Player.h"
 
 BossStage1::BossStage1()
 {
@@ -7,10 +8,13 @@ BossStage1::BossStage1()
 
 BossStage1::~BossStage1()
 {
+	
 }
 
 bool BossStage1::Start()
 {
+	m_player = FindGO<Player>("player");
+
 	//ƒŒƒxƒ‹
 	levelbg.Init(
 		"Assets/level3D/FieldLevel.tkl",
@@ -38,11 +42,31 @@ bool BossStage1::Start()
 	Tree.SetPosition(100.0f, 0.0f, 0.0f);
 	Tree.Update();
 
+	m_tree_Collision = NewGO<CollisionObject>(0);
+	m_tree_Collision->CreateCapsule(
+		Vector3(100.0f, 0.0f, 0.0f),
+		Quaternion::Identity,
+		float(10.0f),
+		float(50.0f)
+	);
+	m_tree_Collision->SetIsEnable(true);
+	m_tree_Collision->SetIsEnableAutoDelete(false);
+	m_tree_Collision->SetName("tree");
+	m_tree_Collision->SetPosition(Vector3(100.0f, 0.0f, 0.0f));
+	m_tree_Collision->Update();
+
+	
+
 	return true;
 }
 
 void BossStage1::Update()
 {
+	if (m_tree_Collision->IsHit(m_player->GetCharacterController())==true)
+	{
+		DeleteGO(m_tree_Collision);
+	}
+
 }
 
 void BossStage1::Render(RenderContext& rc)
