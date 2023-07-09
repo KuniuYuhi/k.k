49005597000b94374cc029eb7b4d3a6f961fc039
azuki <yuhi55.k@gmail.com>
2023-07-09 23:54:47 +0900

    攻撃アニメーション4つできるようにした。アニメーションイベント処理されない

--- k.k/Game/Hero.cpp
+++ k.k/Game/Hero.cpp
@@ -3,6 +3,11 @@
 #include "HeroStateIdle.h"
 #include "HeroStateWalk.h"
 #include "HeroStateRun.h"
+#include "HeroStateAttack_1.h"
+#include "HeroStateAttack_2.h"
+#include "HeroStateAttack_3.h"
+#include "HeroStateAttack_4.h"
+
 
 namespace {
 	int MAXHP = 200;
@@ -34,6 +39,10 @@ bool Hero::Start()
 
 	InitModel();
 
+	//アニメーションイベント用の関数を設定する。
+	m_modelRender.AddAnimationEvent([&](const wchar_t* clipName, const wchar_t* eventName) {
+		OnAnimationEvent(clipName, eventName);
+		});
 
 	return true;
 }
@@ -47,6 +56,15 @@ void Hero::InitModel()
 	m_animationClip[enAnimClip_Walk].SetLoopFlag(true);
 	m_animationClip[enAnimClip_Run].Load("Assets/animData/character/Player/SprintFWD_Battle.tka");
 	m_animationClip[enAnimClip_Run].SetLoopFlag(true);
+	m_animationClip[enAnimClip_Attack_1].Load("Assets/animData/character/Player/Attack1.tka");
+	m_animationClip[enAnimClip_Attack_1].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_2].Load("Assets/animData/character/Player/Attack2.tka");
+	m_animationClip[enAnimClip_Attack_2].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_3].Load("Assets/animData/character/Player/Attack3.tka");
+	m_animationClip[enAnimClip_Attack_3].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_4].Load("Assets/animData/character/Player/Attack4.tka");
+	m_animationClip[enAnimClip_Attack_4].SetLoopFlag(false);
+
 
 	m_modelRender.Init(
 		"Assets/modelData/character/Player/Hero.tkm",
@@ -60,15 +78,27 @@ void Hero::InitModel()
 	m_modelRender.SetScale(m_scale);
 	m_modelRender.Update();
 
+	//ボーンIDの取得
+	m_swordBoonId = m_modelRender.FindBoneID(L"hand_r");
 
-	//m_charaCon.Init(12.0f, 33.0f, g_vec3Zero);
 }
+
+/// <summary>
+/// 毎フレームSetNextAnimationState()を実行
+/// </summary>
 
 void Hero::Update()
 {
+	
 	Move();
+	Attack();
 	ManageState();
 	PlayAnimation();
+
+	if (m_createAttackCollisionFlag == true)
+	{
+		CreateCollision();
+	}
 
 	SetTransFormModel(m_modelRender);
 	m_modelRender.Update();
@@ -76,14 +106,20 @@ void Hero::Update()
 
 void Hero::Move()
 {
-	if (g_pad[0]->IsPress(enButtonA))
+	if (fabsf(m_moveSpeed.x) >= 0.001f || fabsf(m_moveSpeed.z) >= 0.001f)
 	{
-		m_dashFlag = true;
+		if (g_pad[0]->IsPress(enButtonA))
+		{
+			m_dashFlag = true;
+		}
+		else
+		{
+			m_dashFlag = false;
+		}
 	}
-	else
-	{
-		m_dashFlag = false;
-	}
+	
+
+	
 
 	m_moveSpeed = m_player->GetMoveSpeed();
 	m_position = m_player->GetPosition();
@@ -91,9 +127,80 @@ void Hero::Move()
 	Rotation();
 }
 
+void Hero::Attack()
+{
+	//アタックパターンがなしの時
+	//1コンボ
+	if (g_pad[0]->IsTrigger(enButtonY)&& m_enAttackPatternState==enAttackPattern_None)
+	{
+		//攻撃パターン１
+		m_enAttackPatternState = enAttackPattern_1;
+
+		SetNextAnimationState(enAnimationState_Attack_1);
+		return;
+	}
+	//2コンボ
+	if (m_enAttackPatternState == enAttackPattern_1)
+	{
+		if (g_pad[0]->IsTrigger(enButtonY))
+		{
+			m_createAttackCollisionFlag = true;
+
+			m_enAttackPatternState = enAttackPattern_2;
+			//アニメーションイベントあるなら要らない
+			SetNextAnimationState(enAnimationState_Attack_2);
+			return;
+		}
+	}
+	//3コンボ
+	if (m_enAttackPatternState == enAttackPattern_2)
+	{
+		if (g_pad[0]->IsTrigger(enButtonY))
+		{
+			
+			m_enAttackPatternState = enAttackPattern_3;
+			//アニメーションイベントあるなら要らない
+			SetNextAnimationState(enAnimationState_Attack_3);
+			return;
+		}
+	}
+	//4コンボ
+	if (m_enAttackPatternState == enAttackPattern_3)
+	{
+		m_createAttackCollisionFlag = false;
+		if (g_pad[0]->IsTrigger(enButtonY))
+		{
+			m_enAttackPatternState = enAttackPattern_4;
+			//アニメーションイベントあるなら要らない
+			SetNextAnimationState(enAnimationState_Attack_4);
+			return;
+		}
+	}
+}
+
 void Hero::PlayAnimation()
 {
 	m_state->PlayAnimation();
+}
+
+void Hero::CreateCollision()
+{
+	auto AtkCollision = NewGO<CollisionObject>(0,"Attack");
+	
+	//ボックスの当たり判定作成
+	AtkCollision->CreateBox(
+		m_position,
+		Quaternion(90.0f,0.0f,0.0f,1.0f),
+		Vector3(10.0f, 5.0f, 50.0f)
+	);
+
+	//Vector3 swordPos = { 0.0f, 0.0f, -50.0f };
+
+	//剣のボーンのワールド座標を取得
+	Matrix SwordBoonMatrix = m_modelRender.GetBone(m_swordBoonId)->GetWorldMatrix();
+	//SwordBoonMatrix.Apply(swordPos);
+	//AtkCollision->SetPosition(swordPos);
+	AtkCollision->SetWorldMatrix(SwordBoonMatrix);
 }
 
 void Hero::SetNextAnimationState(EnAnimationState nextState)
@@ -103,6 +210,10 @@ void Hero::SetNextAnimationState(EnAnimationState nextState)
 		delete m_state;
 		m_state = nullptr;
 	}
+
+	//アニメーションステートを次のステートに変える
+	m_enAnimationState = nextState;
+
 	switch (m_enAnimationState)
 	{
 	case Hero::enAninationState_Idle:
@@ -117,13 +228,33 @@ void Hero::SetNextAnimationState(EnAnimationState nextState)
 		// 走るステートを作成する。
 		m_state = new HeroStateRun(this);
 		break;
+	case Hero::enAnimationState_Attack_1:
+		//アタック１ステートを作成する。
+		m_state = new HeroStateAttack_1(this);
+		break;
+	case Hero::enAnimationState_Attack_2:
+		//アタック２ステートを作成する。
+		m_state = new HeroStateAttack_2(this);
+		break;
+	case Hero::enAnimationState_Attack_3:
+		//アタック３ステートを作成する。
+		m_state = new HeroStateAttack_3(this);
+		break;
+	case Hero::enAnimationState_Attack_4:
+		//アタック４ステートを作成する。
+		m_state = new HeroStateAttack_4(this);
+		break;
+
 	default:
 		// ここに来たらステートのインスタンス作成処理の追加忘れ。
 		std::abort();
 		break;
 	}
-	m_enAnimationState = nextState;
+
+	
+	
 }
+
 //状態遷移管理
 void Hero::ManageState()
 {
@@ -147,11 +278,90 @@ void Hero::ProcessCommonStateTransition()
 	{
 		SetNextAnimationState(enAninationState_Idle);
 	}
+
+	
+}
+//アタック１の状態遷移処理
+void Hero::OnProcessAttack_1StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		/*if (m_enAttackPatternState == enAttackPattern_2)
+		{
+			SetNextAnimationState(enAnimationState_Attack_2);
+			return;
+		}*/
+
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+
 }
 
-void Hero::OnProcessRunStateTransition()
+void Hero::OnProcessAttack_2StateTransition()
 {
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		
 
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+
+void Hero::OnProcessAttack_3StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+
+void Hero::OnProcessAttack_4StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+
+void Hero::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName)
+{
+	//アタック1のコンボ受付タイムが終わったら
+	if (wcscmp(eventName, L"Attack1_ComboEnd") == 0)
+	{
+		int a = 0;
+
+		if (m_enAttackPatternState == enAttackPattern_2)
+		{
+			SetNextAnimationState(enAnimationState_Attack_2);
+		}
+	}
+
+	//アタック２のコンボ受付タイムが始まったら
+	if (wcscmp(eventName, L"Attack2_ComboStart") == 0)
+	{
+		int a = 1;
+	}
+	//アタック２のコンボ受付タイムが終わったら
+	if (wcscmp(eventName, L"Attack2_ComboEnd") == 0)
+	{
+		int a = 1;
+	}
 }
 
 void Hero::Render(RenderContext& rc)
--- k.k/Game/Hero.h
+++ k.k/Game/Hero.h
@@ -17,22 +17,62 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 
+	void OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName);
+	/// <summary>
+	/// 移動処理
+	/// </summary>
 	void Move();
-
+	/// <summary>
+	/// 攻撃処理
+	/// </summary>
+	void Attack();
+	/// <summary>
+	/// アニメーションの再生
+	/// </summary>
 	void PlayAnimation();
-
+	/// <summary>
+	/// ステート管理
+	/// </summary>
 	void ManageState();
 
 	/// <summary>
-	/// 特定のアニメーションが再生中(true)なら入れ替え可能。
+	/// 攻撃時の当たり判定の作成
+	/// </summary>
+	void CreateCollision();
+
+
+	bool GetAtkCollsionCreateFlag() const
+	{
+		return m_createAttackCollisionFlag;
+	}
+
+	/// <summary>
+	/// 特定のアニメーションが再生中なら入れ替え可能。?
 	/// </summary>
 	/// <returns></returns>
 	bool isAnimationSwappable() const
 	{
-		return m_enAnimationState == enAninationState_Idle &&
-			m_enAnimationState == enAninationState_Walk &&
-			m_enAnimationState == enAninationState_Run;
+		return m_enAnimationState != enAninationState_Idle &&
+			m_enAnimationState != enAninationState_Walk &&
+			m_enAnimationState != enAninationState_Run;
 	}
+
+	/// <summary>
+	/// 特定のアニメーションが再生中なら移動できない
+	/// </summary>
+	/// <returns></returns>
+	bool isAnimationEntable() const
+	{
+		return m_enAnimationState != enAnimationState_Attack_1 &&
+			m_enAnimationState != enAnimationState_Attack_2 &&
+			m_enAnimationState != enAnimationState_Attack_3 /*&&
+			m_enAnimationState != enAnimationState_Attack_4*/;
+	}
+
+	/// <summary>
+	/// モデルレンダーの取得
+	/// </summary>
+	/// <returns></returns>
 	ModelRender& GetModelRender()
 	{
 		return m_modelRender;
@@ -42,13 +82,34 @@ public:
 	/// 共通のステート遷移処理を実行
 	/// </summary>
 	void ProcessCommonStateTransition();
+	/// <summary>
+	/// アタック１のステート遷移処理を実行
+	/// </summary>
+	void OnProcessAttack_1StateTransition();
+	/// <summary>
+	/// アタック2のステート遷移処理を実行
+	/// </summary>
+	void OnProcessAttack_2StateTransition();
+	/// <summary>
+	/// アタック3のステート遷移処理を実行
+	/// </summary>
+	void OnProcessAttack_3StateTransition();
+	/// <summary>
+	/// アタック4のステート遷移処理を実行
+	/// </summary>
+	void OnProcessAttack_4StateTransition();
+
 public:
 	// アニメーションクリップの番号を表す列挙型。
 	enum EnAnimationClip {
 		enAnimClip_Idle,	// 0 : 待機アニメーション
 		enAnimClip_Walk,	// 1 : 歩きアニメーション
 		enAnimClip_Run,		// 2 : 走りアニメーション
-		enAnimClip_Num,		// 3 :アニメーションクリップの数
+		enAnimClip_Attack_1,// 3 : 
+		enAnimClip_Attack_2,// 4 : 
+		enAnimClip_Attack_3,// 5 : 
+		enAnimClip_Attack_4,// 6 : 
+		enAnimClip_Num,		// 7 :アニメーションクリップの数
 	};
 
 private:
@@ -56,21 +117,41 @@ private:
 	enum EnAnimationState {
 		enAninationState_Idle,
 		enAninationState_Walk,
-		enAninationState_Run
+		enAninationState_Run,
+		enAnimationState_Attack_1,
+		enAnimationState_Attack_2,
+		enAnimationState_Attack_3,
+		enAnimationState_Attack_4
+	};
+
+	//攻撃パターンステート
+	enum EnAttackPattern {
+		enAttackPattern_None,
+		enAttackPattern_1,
+		enAttackPattern_2,
+		enAttackPattern_3,
+		enAttackPattern_4,
+		enAttackPattern_End
 	};
 
 public:
 	//アニメーションステートを設定する
 	void SetAnimationState()
 	{
-		m_enAnimationState= enAninationState_Idle;
+		//m_enAnimationState= enAninationState_Idle;
 	}
 
 private:
 
 	
-	void OnProcessRunStateTransition();
+	
+	/// <summary>
+	/// 次のアニメーションステートを作成する。
+	/// </summary>
+	/// <param name="nextState"></param>
 	void SetNextAnimationState(EnAnimationState nextState);
+
+
 	ModelRender m_modelRender;
 
 	Animation m_animation;	// アニメーション
@@ -78,7 +159,13 @@ private:
 
 	EnAnimationState m_enAnimationState = enAninationState_Idle;	//アニメーションステート
 
+	EnAttackPattern m_enAttackPatternState = enAttackPattern_None;
+
 	IHeroState* m_state = nullptr;					//ステートクラス
+
+
+	int m_swordBoonId = -1;		//剣のボーンID取得用変数
+	bool m_createAttackCollisionFlag = false;		//攻撃時に当たり判定を生成するかのフラグ
 
 };
 
