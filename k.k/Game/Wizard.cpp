d4371defac88556c6b5ac943b388239e9a0b921c
azuki <yuhi55.k@gmail.com>
2023-07-11 17:14:01 +0900

    攻撃ステートを適用

--- k.k/Game/Wizard.cpp
+++ k.k/Game/Wizard.cpp
@@ -3,6 +3,11 @@
 #include "WizardStateIdle.h"
 #include "WizardStateWalk.h"
 #include "WizardStateRun.h"
+#include "WizardStateAttack_1.h"
+#include "WizardStateAttack_2_Start.h"
+#include "WizardStateAttack_2_main.h"
+#include "WizardStateAttack_3_Start.h"
+#include "WizardStateAttack_3_main.h"
 
 namespace {
 	int MAXHP = 150;
@@ -42,6 +47,7 @@ bool Wizard::Start()
 
 void Wizard::InitModel()
 {
+	//Attack3_maintain
 	//アニメーションクリップをロードする。
 	m_animationClip[enAnimClip_Idle].Load("Assets/animData/character/Wizard/Idle.tka");
 	m_animationClip[enAnimClip_Idle].SetLoopFlag(true);
@@ -49,6 +55,19 @@ void Wizard::InitModel()
 	m_animationClip[enAnimClip_Walk].SetLoopFlag(true);
 	m_animationClip[enAnimClip_Run].Load("Assets/animData/character/Wizard/Run.tka");
 	m_animationClip[enAnimClip_Run].SetLoopFlag(true);
+	m_animationClip[enAnimClip_Attack_1].Load("Assets/animData/character/Wizard/Attack1.tka");
+	m_animationClip[enAnimClip_Attack_1].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_2_start].Load("Assets/animData/character/Wizard/Attack2_start.tka");
+	m_animationClip[enAnimClip_Attack_2_start].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_2_main].Load("Assets/animData/character/Wizard/Attack2_maintain.tka");
+	m_animationClip[enAnimClip_Attack_2_main].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_3_start].Load("Assets/animData/character/Wizard/Attack3_start.tka");
+	m_animationClip[enAnimClip_Attack_3_start].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_3_main].Load("Assets/animData/character/Wizard/Attack3_maintain.tka");
+	m_animationClip[enAnimClip_Attack_3_main].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Attack_4].Load("Assets/animData/character/Wizard/Attack4.tka");
+	m_animationClip[enAnimClip_Attack_4].SetLoopFlag(false);
+
 
 	m_modelRender.Init(
 		"Assets/modelData/character/Wizard/Wizard.tkm",
@@ -60,13 +79,18 @@ void Wizard::InitModel()
 	//m_modelRender.SetPosition(m_position);
 	m_modelRender.Update();
 
+	//アニメーションイベント用の関数を設定する。
+	m_modelRender.AddAnimationEvent([&](const wchar_t* clipName, const wchar_t* eventName) {
+		OnAnimationEvent(clipName, eventName);
+		});
+
 	//m_charaCon.Init(12.0f, 33.0f, m_position);
 }
 
 void Wizard::Update()
 {
 	Move();
-
+	Attack();
 	ManageState();
 	PlayAnimation();
 
@@ -91,6 +115,22 @@ void Wizard::Move()
 	Rotation();
 }
 
+void Wizard::Attack()
+{
+	if (g_pad[0]->IsTrigger(enButtonY))
+	{
+		m_enAttackPatternState = enAttackPattern_1;
+		SetNextAnimationState(enAnimationState_Attack_1);
+	}
+
+	if (g_pad[0]->IsTrigger(enButtonX))
+	{
+		m_enAttackPatternState = enAttackPattern_3_start;
+		SetNextAnimationState(enAnimationState_Attack_3_start);
+	}
+
+}
+
 void Wizard::PlayAnimation()
 {
 	m_animationState->PlayAnimation();
@@ -109,6 +149,8 @@ void Wizard::SetNextAnimationState(EnAnimationState nextState)
 		m_animationState = nullptr;
 	}
 
+	m_enAnimationState = nextState;
+
 	switch (m_enAnimationState)
 	{
 	case Wizard::enAninationState_Idle:
@@ -123,6 +165,26 @@ void Wizard::SetNextAnimationState(EnAnimationState nextState)
 		//走るステートを作成する。
 		m_animationState = new WizardStateRun(this);
 		break;
+	case Wizard::enAnimationState_Attack_1:
+		//アタック１ステートを作成する。
+		m_animationState = new WizardStateAttack_1(this);
+		break;
+	case Wizard::enAnimationState_Attack_2_start:
+		//アタック２スタートステートを作成する。
+		m_animationState = new WizardStateAttack_2_Start(this);
+		break;
+	case Wizard::enAnimationState_Attack_2_main:
+		//アタック２メインステートを作成する。
+		m_animationState = new WizardStateAttack_2_main(this);
+		break;
+	case Wizard::enAnimationState_Attack_3_start:
+		//アタック３スタートステートを作成する。
+		m_animationState = new WizardStateAttack_3_Start(this);
+		break;
+	case Wizard::enAnimationState_Attack_3_main:
+		//アタック３メインステートを作成する。
+		m_animationState = new WizardStateAttack_3_Main(this);
+		break;
 
 	default:
 		// ここに来たらステートのインスタンス作成処理の追加忘れ。
@@ -130,32 +192,105 @@ void Wizard::SetNextAnimationState(EnAnimationState nextState)
 		break;
 	}
 
-	m_enAnimationState = nextState;
+	
 }
 
 void Wizard::ProcessCommonStateTransition()
 {
-	if (m_dashFlag == true)
-	{
-		SetNextAnimationState(enAninationState_Run);
-		return;
-	}
+	
 
 	if (fabsf(m_moveSpeed.x) >= 0.001f || fabsf(m_moveSpeed.z) >= 0.001f)
 	{
+		if (m_dashFlag == true)
+		{
+			SetNextAnimationState(enAninationState_Run);
+			return;
+		}
+
 		SetNextAnimationState(enAninationState_Walk);
 	}
 	else
 	{
+		m_dashFlag = false;
 		SetNextAnimationState(enAninationState_Idle);
 	}
 }
 
-void Wizard::OnProcessRunStateTransition()
+void Wizard::OnProcessAttack_1StateTransition()
 {
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+void Wizard::OnProcessAttack_2StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//スタートだったら
+		if (m_enAttackPatternState == enAttackPattern_2_start)
+		{
+			m_enAttackPatternState = enAttackPattern_2_main;
+			//メインアニメーションを再生
+			SetNextAnimationState(enAnimationState_Attack_2_main);
+			return;
+		}
+
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+void Wizard::OnProcessAttack_3StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//スタートだったら
+		if (m_enAttackPatternState == enAttackPattern_3_start)
+		{
+			m_enAttackPatternState = enAttackPattern_3_main;
+			//メインアニメーションを再生
+			SetNextAnimationState(enAnimationState_Attack_3_main);
+			return;
+		}
+
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+void Wizard::OnProcessAttack_4StateTransition()
+{
+	//アニメーションの再生が終わったら
+	if (m_modelRender.IsPlayingAnimation() == false)
+	{
+		//攻撃パターンをなし状態にする
+		m_enAttackPatternState = enAttackPattern_None;
+		//共通の状態遷移処理に移行
+		ProcessCommonStateTransition();
+	}
+}
+
+void Wizard::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName)
+{
+	//一段目のアタックのアニメーションが始まったら
+	if (wcscmp(eventName, L"Attack1_Collision_Start") == 0)
+	{
+		int a = 0;
+	}
 }
 
 void Wizard::Render(RenderContext& rc)
 {
 	m_modelRender.Draw(rc);
 }
+
+
