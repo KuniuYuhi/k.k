8658fbd8359b96c5b2adae279c1b0eac65dbb50c
azuki <yuhi55.k@gmail.com>
2023-07-21 11:31:49 +0900

    タイトル→ゲーム→リザルト→タイトルの流れ実装

--- k.k/Game/Game.h
+++ k.k/Game/Game.h
@@ -2,6 +2,12 @@
 #include "Level3DRender.h"
 //#include "Level2DRender.h"
 
+class Player;
+class Lich;
+class BossStage1;
+class GameCamera;
+class ResultSeen;
+
 
 class Game:public IGameObject
 {
@@ -12,6 +18,7 @@ public:
 	bool Start();
 	void Update();
 
+	void GoResult();
 	
 	void SpriteTransform();
 
@@ -22,9 +29,24 @@ public:
 		return m_position;
 	}
 
+	/// <summary>
+	/// ボスがやられたかのフラグを設定
+	/// </summary>
+	/// <param name="flag"></param>
+	void SetDeathBossFlag(bool flag)
+	{
+		m_DeathBossFlag = flag;
+	}
+
 	void Render(RenderContext& rc);
 
 private:
+
+	Player* m_player = nullptr;
+	Lich* m_lich = nullptr;
+	BossStage1* m_bossStage1 = nullptr;
+	GameCamera* m_gameCamera = nullptr;
+	ResultSeen* m_result = nullptr;
 
 	FontRender fontTest;
 	float m_fontScale = 1.0f;
@@ -40,5 +62,8 @@ private:
 	Vector3 m_position = Vector3(0.0f,0.0f,0.0f);
 	Quaternion m_rotation = Quaternion::Identity;
 	Vector3 m_scale = Vector3::One;
+
+	bool m_DeathBossFlag = false;
+	bool m_createResultFlag = false;
 };
 
---
+++ k.k/Game/Title.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "Title.h"
+#include "Game.h"
+
+Title::Title()
+{
+}
+
+Title::~Title()
+{
+}
+
+bool Title::Start()
+{
+    m_titleSprite.Init("Assets/sprite/titleBack.DDS", 1920, 1080);
+   /* m_titleSprite.SetRoundWipe(true);
+    m_titleSprite.SetRoundWipeStartPosition(1920.0f / 2, 1080.0f / 2);
+    m_titleSprite.SetWipeSize(m_wipeSize);
+    m_titleSprite.Update();*/
+
+    return true;
+}
+
+void Title::Update()
+{
+    /*m_titleSprite.SetWipeSize(m_wipeSize);
+    m_wipeSize -= 5.0f;*/
+
+    if (g_pad[0]->IsTrigger(enButtonA))
+    {
+        Game* game = NewGO<Game>(0, "game");
+        DeleteGO(this);
+    }
+}
+
+void Title::Render(RenderContext& rc)
+{
+    m_titleSprite.Draw(rc);
+}
