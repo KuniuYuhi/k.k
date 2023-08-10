b6ff23a0461b80fa7e36568835dd4878d3a414b4
azuki <yuhi55.k@gmail.com>
2023-08-11 00:58:17 +0900

    ゲームマネージャー追加。使っていない

---
+++ k.k/Game/GameManager.h
@@ -0,0 +1,50 @@
+#pragma once
+
+/*
+*@brief 進行状況の管理
+*/
+class GameManager:public Noncopyable
+{
+private:
+	GameManager();
+	~GameManager();
+public:
+	/// <summary>
+		/// インスタンスの作成。
+		/// </summary>
+	static void CreateInstance()
+	{
+		m_instance = new GameManager;
+	}
+	/// <summary>
+	/// インスタンスの破棄。
+	/// </summary>
+	static void DeleteInstance()
+	{
+		delete m_instance;
+	}
+	/// <summary>
+	/// インスタンスを取得。
+	/// </summary>
+	/// <returns></returns>
+	static GameManager* GetInstance()
+	{
+		return m_instance;
+	}
+public:
+	//ゲームのシーン
+	enum EnSeen
+	{
+		enSeen_Tittle,
+		enSeen_Battle,
+		enSeen_GameClear,
+		enSeen_GameOver,
+		enSeen_Result,
+		enSeen_Feed
+	};
+
+
+
+	static GameManager* m_instance;		//唯一のインスタンスのアドレスを記録する変数。
+};
+
