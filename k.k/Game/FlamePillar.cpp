e2aa6ab0050200e21cb2ba4a6c94d8458683ca13
azuki <yuhi55.k@gmail.com>
2023-09-17 01:45:24 +0900

    フレイムピラーをエフェクトにした

--- k.k/Game/FlamePillar.cpp
+++ k.k/Game/FlamePillar.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include "FlamePillar.h"
 #include "Wizard.h"
+#include "InitEffect.h"
+
+namespace {
+    const float CIRCLE_SIZE = 12.0f;
+}
 
 FlamePillar::FlamePillar()
 {
@@ -8,6 +13,14 @@ FlamePillar::FlamePillar()
 
 FlamePillar::~FlamePillar()
 {
+    m_circleEffect->Stop();
+
+    if (m_flamePillarEffect != nullptr)
+    {
+        m_flamePillarEffect->Stop();
+    }
+    
+
     DeleteGO(m_pillarCollision);
 }
 
@@ -32,28 +45,26 @@ bool FlamePillar::Start()
     
     m_model.Update();
 
-    Vector3 pillarPos = m_position;
-    pillarPos.y += 30.0f;
-
-    //当たり判定作成
-    m_pillarCollision = NewGO<CollisionObject>(0, "flamepillar");
-    m_pillarCollision->CreateCapsule(
-        pillarPos,
-        Quaternion::Identity,
-        15.0f,
-        40.0f
-    );
-    //当たり判定はなし
-    //m_pillarCollision->SetIsEnable(false);
-    m_pillarCollision->SetIsEnableAutoDelete(false);
-    m_pillarCollision->SetPosition(pillarPos);
-    m_pillarCollision->Update();
+    Vector3 effectPos = m_position;
+    effectPos.y += 5.0f;
+    //魔法陣エフェクト再生
+    m_circleEffect = NewGO<EffectEmitter>(0);
+    m_circleEffect->Init(InitEffect::enEffect_FlamePillar_Circle);
+    m_circleEffect->Play();
+    m_circleEffect->SetScale(g_vec3One * CIRCLE_SIZE);
+    m_circleEffect->SetPosition(effectPos);
+    m_circleEffect->Update();
 
     return true;
 }
 
 void FlamePillar::Update()
 {
+    if (m_startFlamePllarFlag == true)
+    {
+        StartFlamePillar();
+    }
+
     CalcDamageTime();
     Timer();
 }
@@ -89,7 +100,38 @@ void FlamePillar::CalcDamageTime()
     }
 }
 
+void FlamePillar::StartFlamePillar()
+{
+    Vector3 pillarPos = m_position;
+    pillarPos.y += 30.0f;
+
+    //当たり判定作成
+    m_pillarCollision = NewGO<CollisionObject>(0, "flamepillar");
+    m_pillarCollision->CreateCapsule(
+        pillarPos,
+        Quaternion::Identity,
+        15.0f,
+        40.0f
+    );
+    //当たり判定はなし
+    m_pillarCollision->SetIsEnableAutoDelete(false);
+    m_pillarCollision->SetPosition(pillarPos);
+    m_pillarCollision->Update();
+
+    //エフェクト再生
+    m_flamePillarEffect = NewGO<EffectEmitter>(0);
+    m_flamePillarEffect->Init(InitEffect::enEffect_FlamePillar);
+    m_flamePillarEffect->Play();
+    m_flamePillarEffect->SetScale(g_vec3One * CIRCLE_SIZE);
+    m_flamePillarEffect->SetPosition(m_position);
+    m_flamePillarEffect->Update();
+
+
+    //フレイムピラーを出したのでフラグをfalseにする
+    m_startFlamePllarFlag = false;
+}
+
 void FlamePillar::Render(RenderContext& rc)
 {
-    m_model.Draw(rc);
+    //m_model.Draw(rc);
 }
