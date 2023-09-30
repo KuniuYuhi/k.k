d79dcec6cf9c6f973a665ecb434880524434b338
azuki <yuhi55.k@gmail.com>
2023-09-30 14:54:59 +0900

    値変更

--- k.k/Game/Mushroom.cpp
+++ k.k/Game/Mushroom.cpp
@@ -13,14 +13,14 @@
 #include "Lich.h"
 
 namespace {
-	const float ANGLE = 50.0f;				//視野角
-	const float DISTANCE_TO_PLAYER = 450.0f;			//プレイヤーとの距離
+	const float ANGLE = 60.0f;				//視野角
+	const float DISTANCE_TO_PLAYER = 800.0f;			//プレイヤーとの距離
 	const float ATTACK_RANGE = 45.0f;					//攻撃できる距離
 	const float STAY_RANGR = 45.0f;						//停止する距離
 	const float ATTACK_INTAERVALE_TIME = 1.5f;			//攻撃する間隔
 	const float ANGLE_RANGE = 2.0f;						//移動するアングルの範囲
 	const float POS2_LENGTH = 27.0f;
-	const float ROT_SPEED = 4.3f;
+	const float ROT_SPEED = 4.7f;
 
 	//ステータス
 	int MAXHP = 100;
