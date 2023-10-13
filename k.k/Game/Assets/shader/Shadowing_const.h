24d2318bf4f229a26c8eda789557960f0afec50f
azuki <yuhi55.k@gmail.com>
2023-10-14 00:57:48 +0900

    シャドウイング用定数宣言ヘッダー追加

---
+++ k.k/Game/Assets/shader/Shadowing_const.h
@@ -0,0 +1,9 @@
+//シャドウイング関係の定数をまとめたヘッダーファイル。
+
+#ifndef _SHADOWING_CONST_H_
+#define _SHADOWING_CONST_H_
+
+static const int NUM_SHADW_MAP = 1;//カスケードシャドウなら3枚
+static const int NUM_DIRECTIONAL_LIGHT = 1;     // ディレクションライトの本数
+
+#endif // !_SHADOWING_CONST_H_
