a069b5b35e52269d5b4d5be197a542054d793111
azuki <yuhi55.k@gmail.com>
2023-10-16 17:47:44 +0900

    シャドウマップを３つに変更。

--- k.k/k2EngineLow/MyRenderer.h
+++ k.k/k2EngineLow/MyRenderer.h
@@ -11,8 +11,8 @@ namespace nsK2EngineLow {
 	// シャドウマップ
 	enum {
 		SHADOW_MAP_AREA_NEAR,		//近影範囲のシャドウマップ。
-		//SHADOW_MAP_AREA_MIDDLE,		//中影範囲のシャドウマップ。
-		//SHADOW_MAP_AREA_FAR,		//遠影範囲のシャドウマップ。
+		SHADOW_MAP_AREA_MIDDLE,		//中影範囲のシャドウマップ。
+		SHADOW_MAP_AREA_FAR,		//遠影範囲のシャドウマップ。
 		NUM_SHADOW_MAP
 	};
 
