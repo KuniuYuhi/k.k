90bd7e096b6cc401af66f450a43fd08107229cd8
azuki <yuhi55.k@gmail.com>
2023-10-16 17:50:25 +0900

    レジスタの順番をかえた

--- k.k/Game/Assets/shader/Model_srv_uav_register.h
+++ k.k/Game/Assets/shader/Model_srv_uav_register.h
@@ -10,9 +10,10 @@
 Texture2D<float4> g_albedo : register(t0);		//�A���x�h�}�b�v
 Texture2D<float4>g_normalMap : register(t1);	//�@���}�b�v
 Texture2D<float4>g_specularMap : register(t2);	//�X�y�L�����}�b�v
-Texture2D<float4> g_shadowMap[NUM_SHADOW_MAP] : register(t10);  //�V���h�E�}�b�v�B
-Texture2D<float4>g_toonMap : register(t11);		//�g�D�[���}�b�v
-Texture2D<float4>g_depthTexture : register(t12);//�[�x�e�N�X�`���ɃA�N�Z�X
+Texture2D<float4> g_shadowMap[NUM_SHADOW_MAP] : register(t12);  //�V���h�E�}�b�v�B
+//Texture2D<float4> g_shadowMap[NUM_SHADOW_MAP] : register(t13);  //�V���h�E�}�b�v�B
+Texture2D<float4>g_toonMap : register(t10);		//�g�D�[���}�b�v
+Texture2D<float4>g_depthTexture : register(t11);//�[�x�e�N�X�`���ɃA�N�Z�X
 
 
 #endif // !_MODEL_SRV_UAV_REGISTER_
