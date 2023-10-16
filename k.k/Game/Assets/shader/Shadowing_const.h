8e66a0728633ae48fd5ded537f0ffaf0c7394c29
azuki <yuhi55.k@gmail.com>
2023-10-16 17:49:45 +0900

    シャドウマップを3枚にした

--- k.k/Game/Assets/shader/Shadowing_const.h
+++ k.k/Game/Assets/shader/Shadowing_const.h
@@ -1,9 +1,8 @@
-//�V���h�E�C���O�֌W�̒萔���܂Ƃ߂��w�b�_�[�t�@�C���B
+// シャドウイング関係の定数をまとめたヘッダーファイル。
 
 #ifndef _SHADOWING_CONST_H_
 #define _SHADOWING_CONST_H_
 
-static const int NUM_SHADOW_MAP = 1;//�J�X�P�[�h�V���h�E�Ȃ�3��
-static const int NUM_DIRECTIONAL_LIGHT = 1;     // �f�B���N�V�������C�g�̖{��
+static const int NUM_SHADOW_MAP = 3; // シャドウマップの枚数。
 
 #endif // !_SHADOWING_CONST_H_
