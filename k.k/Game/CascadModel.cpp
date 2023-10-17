13db67b13263f764d39d59e6b006d8e6df9da15b
azuki <yuhi55.k@gmail.com>
2023-10-17 16:40:22 +0900

    新しいモデルのアニメーション再生の確認

--- k.k/Game/CascadModel.cpp
+++ k.k/Game/CascadModel.cpp
@@ -11,18 +11,79 @@ CascadModel::~CascadModel()
 
 bool CascadModel::Start()
 {
-	//シャドウキャスター
-	tea.Init("Assets/modelData/cascadeShadowModel/testModel.tkm",
-		L"Assets/shader/ToonTextrue/lamp_glay.DDS",
-		0, 0, enModelUpAxisZ, true, true, true);
+	g_renderingEngine->SetAmbient(g_vec3One * 1.3f);
 
-	//シャドウレシーバー
+	////シャドウキャスター
+	//tea.Init("Assets/modelData/cascadeShadowModel/testModel.tkm",
+	//	L"Assets/shader/ToonTextrue/lamp_glay.DDS",
+	//	0, 0, enModelUpAxisZ, true, true, true);
+
+
+	m_animationClip[enAnimClip_Idle].Load("Assets/animData/character/Player/NewHero/Idle.tka");
+	m_animationClip[enAnimClip_Idle].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Sprint].Load("Assets/animData/character/Player/NewHero/Sprint.tka");
+	m_animationClip[enAnimClip_Sprint].SetLoopFlag(false);
+	m_animationClip[enAnimClip_DashForward].Load("Assets/animData/character/Player/NewHero/DashForward.tka");
+	m_animationClip[enAnimClip_DashForward].SetLoopFlag(false);
+	m_animationClip[enAnimClip_KnockBack].Load("Assets/animData/character/Player/NewHero/KnockBack.tka");
+	m_animationClip[enAnimClip_KnockBack].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Hit].Load("Assets/animData/character/Player/NewHero/Hit.tka");
+	m_animationClip[enAnimClip_Hit].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Defend].Load("Assets/animData/character/Player/NewHero/Defend.tka");
+	m_animationClip[enAnimClip_Defend].SetLoopFlag(false);
+	m_animationClip[enAnimClip_DefendHit].Load("Assets/animData/character/Player/NewHero/DefendHit.tka");
+	m_animationClip[enAnimClip_DefendHit].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Die].Load("Assets/animData/character/Player/NewHero/Die.tka");
+	m_animationClip[enAnimClip_Die].SetLoopFlag(false);
+	m_animationClip[enAnimClip_ChangeSwordShield].Load("Assets/animData/character/Player/NewHero/ChangeSwordShield.tka");
+	m_animationClip[enAnimClip_ChangeSwordShield].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Win_Start].Load("Assets/animData/character/Player/NewHero/Win_start.tka");
+	m_animationClip[enAnimClip_Win_Start].SetLoopFlag(false);
+	m_animationClip[enAnimClip_Win_Main].Load("Assets/animData/character/Player/NewHero/Win_main.tka");
+	m_animationClip[enAnimClip_Win_Main].SetLoopFlag(false);
+	m_animationClip[enAnimClip_attack1].Load("Assets/animData/character/Player/NewHero/Attack_1.tka");
+	m_animationClip[enAnimClip_attack1].SetLoopFlag(false);
+	m_animationClip[enAnimClip_attack2].Load("Assets/animData/character/Player/NewHero/Attack_2.tka");
+	m_animationClip[enAnimClip_attack2].SetLoopFlag(false);
+	m_animationClip[enAnimClip_attack3].Load("Assets/animData/character/Player/NewHero/Attack_3.tka");
+	m_animationClip[enAnimClip_attack3].SetLoopFlag(false);
+	m_animationClip[enAnimClip_attack4].Load("Assets/animData/character/Player/NewHero/Attack_4.tka");
+	m_animationClip[enAnimClip_attack4].SetLoopFlag(false);
+	m_animationClip[enAnimClip_attack5].Load("Assets/animData/character/Player/NewHero/Attack_5.tka");
+	m_animationClip[enAnimClip_attack5].SetLoopFlag(false);
+
+	Player.Init("Assets/modelData/character/Player/NewHero/Hero_Smile.tkm", 
+		L"Assets/shader/ToonTextrue/lamp_glay.DDS", 
+		m_animationClip, 
+		enAnimClip_Num, 
+		enModelUpAxisZ);
+
+	//Player.PlayAnimation(enAnimClip_Idle);
+	Player.SetAnimationSpeed(0.5f);
+
+	Sword.Init("Assets/modelData/character/Player/NewHero/OneHandSword.tkm",
+		L"Assets/shader/ToonTextrue/lamp_glay.DDS",
+		0,
+		0,
+		enModelUpAxisZ);
+	m_swordBoonId = Player.FindBoneID(L"weaponShield_r");
+
+	Shield.Init("Assets/modelData/character/Player/NewHero/Shield.tkm",
+		L"Assets/shader/ToonTextrue/lamp_glay.DDS",
+		0,
+		0,
+		enModelUpAxisZ);
+	m_shieldBoonId = Player.FindBoneID(L"weaponShield_l");
+
+
+	////シャドウレシーバー
 	bg.Init("Assets/modelData/cascadeShadowModel/bg/bg.tkm",
 		L"Assets/shader/ToonTextrue/lamp_glay.DDS",
 		0, 0, enModelUpAxisZ, false, false, false);
 
 	g_camera3D->SetPosition(0, 100.0f, 350.0f);
 	g_camera3D->SetTarget(0, 0.0f, 0);
+
 
 	return true;
 }
@@ -31,16 +92,48 @@ void CascadModel::Update()
 {
 	MoveCamera();
 
+	
+	
+	if (Player.IsPlayingAnimation() == false) {
+
+		m_playAnimCount++;
+		if (m_playAnimCount == enAnimClip_Num)
+		{
+			m_playAnimCount = 0;
+		}
+
+		//次のアニメーション再生
+		Player.PlayAnimation(m_playAnimCount);
+	}
 
 
 
+
+	Player.Update();
+
+	UpDateWeapon();
 
 }
 
 void CascadModel::Render(RenderContext& rc)
 {
-	tea.Draw(rc);
+	Player.Draw(rc);
+	Sword.Draw(rc);
+	Shield.Draw(rc);
+	//tea.Draw(rc);
 	bg.Draw(rc);
+}
+
+void CascadModel::UpDateWeapon()
+{
+	Matrix swordMt = Player.GetBone(m_swordBoonId)->GetWorldMatrix();
+	Sword.SetWorldMatrix(swordMt);
+
+	Matrix shieldMt = Player.GetBone(m_shieldBoonId)->GetWorldMatrix();
+	Shield.SetWorldMatrix(shieldMt);
+
+	Sword.Update();
+	Shield.Update();
 }
 
 void CascadModel::MoveCamera()
--- k.k/Game/CascadModel.h
+++ k.k/Game/CascadModel.h
@@ -9,15 +9,47 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 
+	void UpDateWeapon();
+
 	void MoveCamera();
+
+	// アニメーションクリップの番号を表す列挙型。
+	enum EnAnimationClip {
+		enAnimClip_Idle,
+		enAnimClip_Sprint,
+		enAnimClip_DashForward,
+		enAnimClip_KnockBack,
+		enAnimClip_Hit,
+		enAnimClip_Defend,
+		enAnimClip_DefendHit,
+		enAnimClip_Die,
+		enAnimClip_ChangeSwordShield,
+		enAnimClip_Win_Start,
+		enAnimClip_Win_Main,
+		enAnimClip_attack1,
+		enAnimClip_attack2,
+		enAnimClip_attack3,
+		enAnimClip_attack4,
+		enAnimClip_attack5,
+
+		enAnimClip_Num
+	};
 
 private:
 	ModelRender tea;
 	ModelRender bg;
 
-	
 
+	Animation	m_animation;				// アニメーション
+	AnimationClip	m_animationClip[enAnimClip_Num];// アニメーションクリップ 
+	ModelRender Player;
 
+	ModelRender Sword;
+	int m_swordBoonId = -1;
 
+	ModelRender Shield;
+	int m_shieldBoonId = -1;
+
+	int m_playAnimCount = 0;
 };
 
