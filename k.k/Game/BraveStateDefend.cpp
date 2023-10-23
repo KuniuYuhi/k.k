a4bf1853c2409f902aaa9671891a5042f0ecd15a
azuki <yuhi55.k@gmail.com>
2023-10-23 10:06:03 +0900

    防御タイプによって処理を変更するようにした

--- k.k/Game/BraveStateDefend.cpp
+++ k.k/Game/BraveStateDefend.cpp
@@ -2,14 +2,24 @@
 #include "BraveStateDefend.h"
 #include "Brave.h"
 
+namespace {
+	const int DEFENCE = 0;
+	const int AVOID = 1;
+}
+
 void BraveStateDefend::ManageState()
 {
-	//Yボタンを離したら防御状態解除
-	if (g_pad[0]->IsPress(enButtonY) != true)
+	//武器によって違う処理
+	switch (m_brave->GetMainWeaponDefendTipe())
 	{
-		//防御状態が解除されたのでアクションフラグをfalseにする
-		m_brave->SetIsActionFlag(false);
-		m_brave->ProcessCommonStateTransition();
+	case DEFENCE:
+		ProcessDefence();
+		break;
+	case AVOID:
+		ProcessAvoid();
+		break;
+	default:
+		break;
 	}
 }
 
@@ -20,4 +30,27 @@ void BraveStateDefend::PlayAnimation()
 		0.2f
 	);
 
+}
+
+void BraveStateDefend::ProcessDefence()
+{
+	//Yボタンを離したら防御状態解除
+	if (g_pad[0]->IsPress(enButtonY) != true)
+	{
+		//防御状態が解除されたのでアクションフラグをfalseにする
+		m_brave->SetAllInfoAboutActionFlag(false);
+		m_brave->ProcessCommonStateTransition();
+	}
+}
+
+void BraveStateDefend::ProcessAvoid()
+{
+	//前進するフラグが立っている間は処理
+	if (m_brave->GetMoveForwardFlag() == true)
+	{
+		//向いている方向に移動
+		m_brave->MoveForward(m_brave->GetAvoidSpeed());
+	}
+
+	m_brave->ProcessDefendStateTransition();
 }
