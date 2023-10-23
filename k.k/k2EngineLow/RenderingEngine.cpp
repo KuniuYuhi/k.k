583840848e3eea7e656a72de953cb3daf1216fb2
azuki <yuhi55.k@gmail.com>
2023-10-23 10:02:32 +0900

    コメントアウト行を削除

--- k.k/k2EngineLow/RenderingEngine.cpp
+++ k.k/k2EngineLow/RenderingEngine.cpp
@@ -168,7 +168,7 @@ namespace nsK2EngineLow {
 		
 		m_shadowMapRenders.Render(
 			rc,
-			GetLightCamera()/*m_deferredLightingCB.m_light.directionalLight.lightDirection*/,
+			GetLightCamera(),
 			m_renderObjects
 		);
 
