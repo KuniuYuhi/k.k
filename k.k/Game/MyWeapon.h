668df82238e9f56f00aa2b469554bd5fba8e57a7
azuki <yuhi55.k@gmail.com>
2023-10-31 11:41:13 +0900

    武器用の変数宣言ファイル追加

---
+++ k.k/Game/MyWeapon.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/// <summary>
+/// •Ší‚Ìí—Ş
+/// </summary>
+enum EnWeaponType
+{
+	enWeaponType_SwordShield,
+	enWeaponType_OneHandSword,
+	enWeaponType_Bow,
+	enWeaponType_DoubleSwords,
+	enWeaponType_Num
+};
