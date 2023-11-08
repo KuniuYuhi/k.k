695ee9a415a2535a99134e32c3404dbcaa0f664f
azuki <yuhi55.k@gmail.com>
2023-11-09 00:44:28 +0900

    インターフェースの実装にした

--- k.k/Game/IWeapon.cpp
+++ k.k/Game/IWeapon.cpp
@@ -1,25 +1,3 @@
 #include "stdafx.h"
 #include "IWeapon.h"
 
-IWeapon::IWeapon()
-{
-}
-
-IWeapon::~IWeapon()
-{
-}
-
-void IWeapon::ReverseWeaponState()
-{
-	//現在の武器の状態ステートが装備状態なら収納状態ステートに変更する
-	if (m_enWeaponState == enWeaponState_Armed)
-	{
-		m_enWeaponState = enWeaponState_Stowed;
-	}
-	//現在の武器の状態ステートが収納状態なら装備状態ステートに変更する
-	else if (m_enWeaponState == enWeaponState_Stowed)
-	{
-		SetStowedFlag(false);
-		m_enWeaponState = enWeaponState_Armed;
-	}
-}
--- k.k/Game/IWeapon.h
+++ k.k/Game/IWeapon.h
@@ -11,11 +11,11 @@ public:
 	/// <summary>
 	/// コンストラクタ。
 	/// </summary>
-	IWeapon();
+	IWeapon(){}
 	/// <summary>
 	/// デストラクタ
 	/// </summary>
-	virtual ~IWeapon();
+	virtual ~IWeapon(){}
 
 	/// <summary>
 	/// 武器の状態ステート
@@ -56,166 +56,6 @@ public:
 	/// </summary>
 	virtual void MoveStowed() = 0;
 
-	/// <summary>
-	/// 武器の当たり判定
-	/// </summary>
-	/// <returns></returns>
-	virtual bool IsHitCollision()
-	{
-		return false;
-	};
-	/// <summary>
-	/// スキル攻撃処理
-	/// </summary>
-	virtual void ProcessSkillAttack()
-	{
-	}
 
-	/// <summary>
-	/// 攻撃やスキル時のジャンプの速度の取得
-	/// </summary>
-	/// <returns></returns>
-	virtual float GetJampSpeed()
-	{
-		return 0;
-	}
-
-	///////////////////////////////////////////////////////////////////////////
-	///その他の関数
-	///////////////////////////////////////////////////////////////////////////
-	/// <summary>
-	/// 武器の状態ステートを逆の状態に変える。
-	/// </summary>
-	/// <param name="NowWeaponState">現在の武器の状態ステート</param>
-	void ReverseWeaponState();
-	/// <summary>
-	/// 武器の状態ステートを設定
-	/// </summary>
-	/// <param name="nextWeaponState"></param>
-	void SetWeaponState(EnWeaponState enWeaponState)
-	{
-		m_enWeaponState = enWeaponState;
-	}
-	/// <summary>
-	/// 武器の状態ステートを返す
-	/// </summary>
-	/// <returns></returns>
-	EnWeaponState& GetWeaponState()
-	{
-		return m_enWeaponState;
-	}
-
-	/// <summary>
-	/// 武器の攻撃力の設定
-	/// </summary>
-	/// <param name="power"></param>
-	void SetPower(int power)
-	{
-		m_power = power;
-	}
-	/// <summary>
-	/// 武器の攻撃力を取得
-	/// </summary>
-	/// <returns></returns>
-	const int& GetPower() const
-	{
-		return m_power;
-	}
-
-	/// <summary>
-	/// 座標を設定
-	/// </summary>
-	/// <param name="position"></param>
-	void SetPosition(Vector3 position)
-	{
-		m_position = position;
-	}
-	/// <summary>
-	/// 座標を取得
-	/// </summary>
-	/// <returns></returns>
-	const Vector3& GetPosition() const
-	{
-		return m_position;
-	}
-
-	/// <summary>
-	/// 収納状態フラグを設定
-	/// </summary>
-	/// <param name="flag"></param>
-	void SetStowedFlag(bool flag)
-	{
-		m_stowedFlag = flag;
-	}
-	/// <summary>
-	/// 収納状態フラグを取得
-	/// </summary>
-	/// <returns></returns>
-	const bool& GetStowedFlag() const
-	{
-		return m_stowedFlag;
-	}
-
-	/// <summary>
-	/// 武器の防御タイプを取得
-	/// </summary>
-	/// <returns></returns>
-	const EnDefendTipe& GetEnDefendTipe() const
-	{
-		return m_enDefendTipe;
-	}
-	/// <summary>
-	/// 多段ヒット攻撃がヒットしたかのフラグを設定
-	/// </summary>
-	/// <param name="flag"></param>
-	virtual void SetHittableFlag(bool flag)
-	{
-	};
-	/// <summary>
-	/// 多段ヒット攻撃がヒットしたかのフラグを取得
-	/// </summary>
-	/// <returns></returns>
-	virtual const bool& GetHittableFlag() const
-	{
-		return true;
-	}
-
-protected:
-
-	/// <summary>
-	/// 武器の防御タイプを設定
-	/// </summary>
-	/// <returns></returns>
-	void SetEnDefendTipe(EnDefendTipe defendTipe)
-	{
-		m_enDefendTipe = defendTipe;
-	}
-	/// <summary>
-	/// 武器の攻撃力を設定
-	/// </summary>
-	/// <param name="power"></param>
-	void SetWeaponPower(int power)
-	{
-		m_power = power;
-	}
-
-protected:
-	Brave* m_brave = nullptr;
-
-	EnWeaponState m_enWeaponState = enWeaponState_None;
-
-	EnDefendTipe m_enDefendTipe = enDefendTipe_None;
-
-	int m_power = 0;		//武器の攻撃力
-
-	Vector3 m_position = g_vec3Zero;	//自身の座標
-
-	bool m_stowedFlag = false;		//収納状態フラグ。処理しないようにするためのフラグ
-
-
-	//派生クラスで必要なもの
-	//ワールド座標
-	//武器を持たせるボーンID
-	//武器をしまった時のボーンID
 };
 
