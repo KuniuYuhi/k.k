41de049b7d4b6407bd3c0ad1bc3f474440650a05
azuki <yuhi55.k@gmail.com>
2023-11-21 18:40:17 +0900

    ポストエフェクトの被写界深度の処理追加

--- k.k/k2EngineLow/PostEffect.cpp
+++ k.k/k2EngineLow/PostEffect.cpp
@@ -2,21 +2,52 @@
 #include "PostEffect.h"
 
 namespace nsK2EngineLow {
-	void PostEffect::Init(RenderTarget& mainRenderTarget)
+	void PostEffect::Init(
+		RenderTarget& mainRenderTarget,
+		RenderTarget& zprepassRenderTarget
+	)
 	{
-		InitBloom(mainRenderTarget);
+		//ブルームの初期化処理
+		m_bloom.Init(mainRenderTarget);
+		//被写界深度の初期化処理
+		m_depthOfFeild.Init(mainRenderTarget, zprepassRenderTarget);
 	}
 
 	void PostEffect::Render(RenderContext& rc, RenderTarget& mainRenderTarget)
 	{
-		m_bloom.OnRender(rc,mainRenderTarget);
+		BeginGPUEvent("Bloom");
+		//ブルーム
+		OnRenderBloom(rc, mainRenderTarget);
+		EndGPUEvent();
 
+		g_renderingEngine->SetMainRenderTargetAndDepthStencilBuffer(rc);
+
+		BeginGPUEvent("Dof");
+		//被写界深度
+		OnRenderDof(rc, mainRenderTarget);
+		EndGPUEvent();
+		
+
+		//メインレンダーターゲットをPRESENTからRENDERTARGETへ。
+		rc.WaitUntilToPossibleSetRenderTarget(mainRenderTarget);
 		EffectEngine::GetInstance()->Draw();
+		//メインレンダーターゲットをTARGETからPRESENTへ。
+		rc.WaitUntilFinishDrawingToRenderTarget(mainRenderTarget);
+
 	}
 
-	void PostEffect::InitBloom(RenderTarget& mainRenderTarget)
+	void PostEffect::OnRenderBloom(RenderContext& rc, RenderTarget& mainRenderTarget)
 	{
-		m_bloom.Init(mainRenderTarget);
+		//ブルーム
+		m_bloom.OnRender(rc, mainRenderTarget);
 	}
+
+	void PostEffect::OnRenderDof(RenderContext& rc, RenderTarget& mainRenderTarget)
+	{
+		//被写界深度
+		m_depthOfFeild.OnRender(rc, mainRenderTarget);
+	}
+
+	
 
 }
