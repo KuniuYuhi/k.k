f392bfdda42faf78c2fae7f502a36f6630ab4537
azuki <yuhi55.k@gmail.com>
2023-11-28 14:13:20 +0900

    リストにアクションを追加

--- k.k/Game/SummonerActionList.h
+++ k.k/Game/SummonerActionList.h
@@ -6,6 +6,7 @@
 enum EnActionName
 {
 	enActionName_Idle,			//待機
+	enActionName_Walk,			//歩き
 	enActionName_Darkball,		//ダークボール(遠距離攻撃)
 	enActionName_DarkWall,		//ダークウォール(近距離攻撃)
 	enActionName_KnockBack,		//ノックバック攻撃(近距離攻撃)
@@ -13,4 +14,10 @@ enum EnActionName
 	enActionName_NormalAttack_2,//通常攻撃２(近距離攻撃)
 	enActionName_NormalAttack_3,//通常攻撃３(近距離攻撃)
 	enActionName_Command,		//モブに号令をかける
+	enActionName_Summon,		//召喚
+	enActionName_CriticalHit,	//スーパーアーマー割られた
+	enActionName_Die,			//やられた
+	enActionName_Victory,		//勝利
+	enActionName_Angry,			//怒りモード
+	enActionName_Warp,			//ワープ(逃げる)
 };
