1331742f0539130dd285df073a6f60d1a2a24471
azuki <yuhi55.k@gmail.com>
2023-12-04 00:27:45 +0900

    ゲーム進行ステートを設定する関数追加。勝敗ステートを設定する関数追加。

--- k.k/Game/GameManager.cpp
+++ k.k/Game/GameManager.cpp
@@ -1,22 +1,98 @@
 #include "stdafx.h"
 #include "GameManager.h"
+#include "CharactersInfoManager.h"
 
+#include "BattleStart.h"
+#include "Player.h"
+#include "Lich.h"
 
+namespace {
+	const float SECOND_MAX = 60.0f;
+}
 
 GameManager* GameManager::m_instance = nullptr;
 
-GameManager::GameManager()
+GameManager::GameManager(EnGameSeenState startGameSeenState)
 {
 	if (m_instance != nullptr) {
 		//インスタンスがすでに作られている。
 		std::abort();
 	}
 	m_instance = this;
+	//ゲーム開始時のステートを設定
+	m_enGameSeenState = startGameSeenState;
 }
 
 GameManager::~GameManager()
 {
+	//キャラ情報マネージャー削除
+	CharactersInfoManager::DeleteInstance();
 
+	m_instance = nullptr;
 }
+
+void GameManager::Init()
+{
+	//キャラクターの情報マネージャーの生成
+	CharactersInfoManager::CreateInstance();
+}
+
+void GameManager::Execute()
+{
+
+
+	CalcTimeLimmit();
+}
+
+void GameManager::CreateBattleStartClass()
+{
+	//バトルスタートクラス生成
+	BattleStart* battleStart = NewGO<BattleStart>(0, "battlestart");
+}
+
+void GameManager::CreatePlayerClass()
+{
+	Player* player = NewGO<Player>(0, "player");
+	CharactersInfoManager::GetInstance()->SetPlayerInstance(player);
+}
+
+void GameManager::CreateBoss()
+{
+	Lich* lich = NewGO<Lich>(0, "lich");
+	CharactersInfoManager::GetInstance()->SetLichInstance(lich);
+}
+
+
+
+void GameManager::CalcTimeLimmit()
+{
+
+
+	//0秒以下なら
+	if (m_second <= 0) {
+		//1分減らす
+		m_minute--;
+		//もし0分なら、秒も0にする
+		if (m_minute < 0) {
+			m_second = 0.0f;
+			m_minute = 0.0f;
+			//制限時間に達した
+			//ゲームオーバーステートを設定
+			SetGameSeenState(enGameSeenState_GameOver);
+		}
+		//60秒に戻す
+		else
+		{
+			m_second = SECOND_MAX;
+		}
+	}
+	else
+	{
+		//秒を減らす
+		m_second -= g_gameTime->GetFrameDeltaTime();
+	}
+}
+
+
 
 
--- k.k/Game/GameManager.h
+++ k.k/Game/GameManager.h
@@ -8,16 +8,47 @@
 
 class GameManager :public Noncopyable
 {
+public:
+	//////////////////////////////////////////////////////////////
+	//構造体、列挙型の宣言
+	//////////////////////////////////////////////////////////////
+	//ゲームのシーン
+	enum EnGameSeenState
+	{
+		enGameSeenState_Fade,
+		enGameSeenState_GameStart,
+		enGameSeenState_AppearanceBoss,
+		enGameSeenState_Game,
+		enGameSeenState_Pause,
+		enGameSeenState_GameOver,
+		enGameSeenState_GameClear
+	};
+
+	enum EnOutComeState
+	{
+		enOutComeState_None,
+		enOutComeState_PlayerWin,
+		enOutComeState_PlayerLose,
+	};
+
+
 private:
-	GameManager();
+	GameManager(EnGameSeenState startGameSeenState);
 	~GameManager();
 public:
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Init();
+
+
 	/// <summary>
 	/// インスタンスの作成。
 	/// </summary>
-	static void CreateInstance()
+	static void CreateInstance(EnGameSeenState startGameSeenState)
 	{
-		m_instance = new GameManager;
+		m_instance = new GameManager(startGameSeenState);
 	}
 	/// <summary>
 	/// インスタンスの破棄。
@@ -36,23 +67,98 @@ public:
 		return m_instance;
 	}
 
+	/// <summary>
+	/// ゲームシーンステートを設定
+	/// </summary>
+	/// <param name="setGameSeenState"></param>
+	void SetGameSeenState(EnGameSeenState setGameSeenState)
+	{
+		m_enGameSeenState = setGameSeenState;
+	}
+	/// <summary>
+	/// ゲームシーンステートを設定
+	/// </summary>
+	/// <returns></returns>
+	const EnGameSeenState& GetGameSeenState() const
+	{
+		return m_enGameSeenState;
+	}
+
+	/// <summary>
+	/// 勝敗結果ステートを設定
+	/// </summary>
+	/// <param name="outComeState"></param>
+	void SetOutComeState(EnOutComeState outComeState)
+	{
+		m_enOutComeState = outComeState;
+	}
+	/// <summary>
+	/// 勝敗結果ステートを取得
+	/// </summary>
+	/// <returns></returns>
+	const EnOutComeState& GetOutComeState() const
+	{
+		return m_enOutComeState;
+	}
+
+	/// <summary>
+	/// 毎フレームの更新処理
+	/// </summary>
+	void Execute();
+
+	/// <summary>
+	/// バトルスタートクラスの作成
+	/// </summary>
+	void CreateBattleStartClass();
+	/// <summary>
+	/// プレイヤークラス生成
+	/// </summary>
+	void CreatePlayerClass();
+	/// <summary>
+	/// ボスクラス生成
+	/// </summary>
+	void CreateBoss();
+
+
+
+
+	/// <summary>
+	/// 制限時間の計算
+	/// </summary>
+	void CalcTimeLimmit();
+
+	/// <summary>
+	/// 秒の取得
+	/// </summary>
+	/// <returns></returns>
+	const int GetMinute() const
+	{
+		return m_minute;
+	}
+	/// <summary>
+	/// 秒の取得
+	/// </summary>
+	/// <returns></returns>
+	const int GetSecond() const
+	{
+		return m_second;
+	}
+
 
 public:
-	//ゲームのシーン
-	enum EnSeenState
-	{
-		enSeen_Tittle,
-		enSeen_Battle,
-		enSeen_GameClear,
-		enSeen_GameOver,
-		enSeen_Result,
-		enSeen_Feed
-	};
+	
+	EnGameSeenState m_enGameSeenState = enGameSeenState_GameStart;
+	EnOutComeState m_enOutComeState = enOutComeState_None;
 
 
 	static GameManager* m_instance;		//唯一のインスタンスのアドレスを記録する変数。
 
+private:
+	float m_minute = 3.0f;			//制限時間
+	float m_second = 0.0f;
 
+	bool m_playerWinFlag = false;
+	bool m_playerLoseFlag = false;
 
 };
 
