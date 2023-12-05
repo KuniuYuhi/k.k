81b4f891c52a88fe9d8187a526f922bbd1298531
azuki <yuhi55.k@gmail.com>
2023-12-05 11:32:40 +0900

    カメラの処理やフェードインまでのバトルスタートの間の流れをこのクラスだけで終わらせるようにした

--- k.k/Game/BattleStart.cpp
+++ k.k/Game/BattleStart.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "BattleStart.h"
+#include "Fade.h"
+#include "CharactersInfoManager.h"
 
 namespace {
     const Vector3 BATTLE_TEXT_START_POS = { 0.0f,336.0f,0.0f };
@@ -8,6 +10,9 @@ namespace {
     const Vector3 START_TEXT_START_POS = { 0.0f,-336.0f,0.0f };
     const Vector3 START_TEXT_END_POS = { -1650.0f,-336.0f,0.0f };
 
+
+    const float TARGETPOS_GAMESTART_YUP = 38.0f;
+    const float STARTCAMERA_YUP = 60.0f;
 }
 
 BattleStart::BattleStart()
@@ -20,6 +25,17 @@ BattleStart::~BattleStart()
 
 bool BattleStart::Start()
 {
+    //フェードのインスタンスを探す
+    m_fade = FindGO<Fade>("fade");
+
+    //ばねカメラの初期化。
+    m_springCamera.Init(
+        *g_camera3D,		//ばねカメラの処理を行うカメラを指定する。
+        1000.0f,			//カメラの移動速度の最大値。
+        false,				//カメラと地形とのあたり判定を取るかどうかのフラグ。trueだとあたり判定を行う。
+        1.0f				//カメラに設定される球体コリジョンの半径。第３引数がtrueの時に有効になる。
+    );
+
     InitSprite();
     //バトルスタートの音再生
     g_soundManager->InitAndPlaySoundSource(enSoundName_BattleStart, g_soundManager->GetBGMVolume());
@@ -28,6 +44,14 @@ bool BattleStart::Start()
 
 void BattleStart::Update()
 {
+    if (m_fadeStartFlag != true)
+    {
+        CalcCameraZoomOutTime();
+    }
+    
+    //ゲームスタート時のカメラ処理
+    GameStartCamera();
+
     //見えるようになったらもうやらない
     if (m_calcAlphaEndFlag != true)
     {
@@ -87,4 +111,57 @@ void BattleStart::InitSprite()
     m_startTextRender.Init("Assets/sprite/InGame/BattleStart/Start.DDS", 1236, 265);
     SettingSpriteRender(m_startTextRender, START_TEXT_START_POS, g_vec3One, g_quatIdentity);
 
+}
+
+void BattleStart::GameStartCamera()
+{
+    //注視点の計算
+    m_target =
+        CharactersInfoManager::GetInstance()->GetPlayerInstance()->GetPosition();
+    m_target.y += TARGETPOS_GAMESTART_YUP;
+    //前方向の取得
+    Vector3 CameraPosXZ = 
+        CharactersInfoManager::GetInstance()->GetPlayerInstance()->GetForward();
+    CameraPosXZ.y = 0.0f;
+
+    //XZ方向の
+    CameraPosXZ *= 100.0f + m_count;
+    //Y方向の
+    Vector3 CameraPosY = Vector3::AxisY;
+    CameraPosY *= STARTCAMERA_YUP + (m_count * 0.5f);
+
+    //カメラの座標
+    Vector3 newCameraPos = CameraPosXZ + CameraPosY;
+
+    Vector3 finalCameraPos = newCameraPos + m_target;
+
+    //視点と注視点を設定
+    m_springCamera.SetTarget(m_target);
+    m_springCamera.SetPosition(finalCameraPos);
+
+    //カメラの更新。
+    m_springCamera.Update();
+
+    m_count++;
+}
+
+bool BattleStart::CalcCameraZoomOutTime()
+{
+    if (m_cameraZoomOutTimer > m_cameraZoomOutTime)
+    {
+        //フェードイン開始
+        //フェードイン仕切ったらゲーム側がゲームスタートクラスを削除する
+        m_fade->StartFadeIn(3.0f);
+        m_fadeStartFlag = true;
+        return true;
+    }
+    else
+    {
+        //タイマーを加算
+        m_cameraZoomOutTimer += g_gameTime->GetFrameDeltaTime();
+    }
+
+   
+
+    return false;
 }
--- k.k/Game/BattleStart.h
+++ k.k/Game/BattleStart.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#include "CameraCollisionSolver.h"
+#include "SpringCamera.h"
+
+class Fade;
+
 class BattleStart:public IGameObject
 {
 public:
@@ -9,11 +15,33 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 
+	/// <summary>
+	/// 画像の移動処理
+	/// </summary>
+	/// <param name="spriteRender"></param>
+	/// <param name="startPos"></param>
+	/// <param name="endPos"></param>
 	void MoveText(SpriteRender& spriteRender,Vector3 startPos,Vector3 endPos);
 
+	/// <summary>
+	/// 透明化
+	/// </summary>
 	void CalcAlpha();
-
+	/// <summary>
+	/// 画像の初期化
+	/// </summary>
 	void InitSprite();
+
+	/// <summary>
+	/// ゲームスタート時のカメラ
+	/// </summary>
+	void GameStartCamera();
+
+	/// <summary>
+	/// カメラのズームアウトタイムの計算
+	/// </summary>
+	/// <returns></returns>
+	bool CalcCameraZoomOutTime();
 
 	/// <summary>
 	/// スプライトの座標、サイズ、回転を設定
@@ -32,14 +60,27 @@ public:
 	}
 
 private:
+	Fade* m_fade = nullptr;
+
 	SpriteRender m_battleTextRender;
 	SpriteRender m_startTextRender;
 
+	CameraCollisionSolver	m_cameraCollisionSolver;
+	SpringCamera			m_springCamera;
+
+	Vector3				m_target = Vector3::Zero;		//カメラ注視点
 	Vector4 m_color = g_vec4White;
 
 	float m_timer = 0.0f;
 
 	float m_alpha = 0.0f;
 	bool m_calcAlphaEndFlag = false;
+
+	bool m_fadeStartFlag = false;
+
+	const float					m_cameraZoomOutTime = 2.0f;
+	float						m_cameraZoomOutTimer = 0.0f;
+
+	float m_count = 1.0f;
 };
 
