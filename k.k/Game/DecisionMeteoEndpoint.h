be1aab2f7b437fc3c863de8951022d0ccd87556b
azuki <yuhi55.k@gmail.com>
2023-12-14 11:46:56 +0900

    メテオの終点を決める関数追加。

---
+++ k.k/Game/DecisionMeteoEndpoint.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace {
+	const int LENGTH = 600.0f;
+}
+
+//メテオの終点の計算
+
+/// <summary>
+/// メテオの終点の取得。プレイヤーの周りに落とす
+/// </summary>
+/// <param name="targetCenterPosition">メテオを落とす中心の座標</param>
+/// <returns></returns>
+Vector3 GetMeteoTargetPosition(Vector3 targetCenterPosition)
+{
+	while (true)
+	{
+		Vector3 finalPos = targetCenterPosition;
+		//中心から300の範囲内でランダムな値を決める
+		float X = rand() % (LENGTH * 2 + 1) - (LENGTH);
+		float Z = rand() % (LENGTH * 2 + 1) - (LENGTH);
+
+		finalPos.x += X;
+		finalPos.z += Z;
+		return finalPos;
+		/*if (finalPos.Length() < LENGTH)
+		{
+			
+		}
+		else
+		{
+			continue;
+		}*/
+	}
+}
