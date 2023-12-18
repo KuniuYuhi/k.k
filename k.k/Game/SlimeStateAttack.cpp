df90db00067372edb12b4a625182fcae276e0b8d
azuki <yuhi55.k@gmail.com>
2023-12-18 17:47:03 +0900

    変数変更。当たり判定生成の条件文追加。

--- k.k/Game/SlimeStateAttack.cpp
+++ k.k/Game/SlimeStateAttack.cpp
@@ -4,10 +4,16 @@
 
 void SlimeStateAttack::ManageState()
 {
+	//当たり判定生成フラグがセットされていたら当たり判定生成
+	if (m_slime->GetCreateAttackCollisionFlag() == true)
+	{
+		m_slime->CreateCollision();
+	}
+
 	m_slime->OnProcessAttack_1StateTransition();
 }
 
 void SlimeStateAttack::PlayAnimation()
 {
-	m_slime->GetModelRender().PlayAnimation(Slime::enAnimClip_Attack_1);
+	m_slime->GetModelRender().PlayAnimation(enAnimationClip_Attack);
 }
