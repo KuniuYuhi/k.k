cc509ddb03ec9e4ccd29fcc153efe5fdae4a96dd
azuki <yuhi55.k@gmail.com>
2023-12-19 15:50:40 +0900

    変数名変更。当たり判定生成処理

--- k.k/Game/MushroomStateAttack_1.cpp
+++ k.k/Game/MushroomStateAttack_1.cpp
@@ -4,10 +4,16 @@
 
 void MushroomStateAttack_1::ManageState()
 {
+	//当たり判定生成フラグがセットされていたら当たり判定生成
+	if (m_mushroom->GetCreateAttackCollisionFlag() == true)
+	{
+		m_mushroom->CreateCollision();
+	}
+
 	m_mushroom->OnProcessAttack_1StateTransition();
 }
 
 void MushroomStateAttack_1::PlayAnimation()
 {
-	m_mushroom->GetModelRender().PlayAnimation(Mushroom::enAnimClip_Attack_1);
+	m_mushroom->GetModelRender().PlayAnimation(enAnimationClip_Attack);
 }
