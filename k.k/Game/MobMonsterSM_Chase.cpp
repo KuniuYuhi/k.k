0f672ebbe530e01a54f4b8024cd9563c1b1527a0
azuki <yuhi55.k@gmail.com>
2024-01-02 00:31:57 +0900

    追跡ステートマシンを追加。モブモンスターステートマシンの派生クラスにした

---
+++ k.k/Game/MobMonsterSM_Chase.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include "MobMonsterSM_Chase.h"
+#include "MobMonster.h"
+
+using namespace MobMonsterInfo;
+
+void MobMonsterSM_Chase::Execute()
+{
+	//行動を決める
+	ProcessDecideAction();
+}
+
+void MobMonsterSM_Chase::ProcessDecideAction()
+{
+	//一定のアニメーション中は処理しない
+	if (m_mobMonster->IsAttackEnable() != true)
+	{
+		return;
+	}
+	if (m_mobMonster->isAnimationEnable() != true)
+	{
+		return;
+	}
+
+	DecideNextAction();
+}
+
+void MobMonsterSM_Chase::DecideNextAction()
+{
+	if (m_mobMonster->IsFoundPlayerFlag() == true)
+	{
+		//プレイヤーを見つけたら追いかける
+		ProcessChase();
+	}
+	else
+	{
+		//プレイヤーを見失ったらステートマシンを切り替える
+		m_mobMonster->SetNextStateMachine(enStateMachineState_Patrol);
+	}
+
+}
+
+void MobMonsterSM_Chase::ProcessAttackState()
+{
+	//まず攻撃ができるか
+	if (m_mobMonster->IsProcessAttackEnable() == true)
+	{
+		//スキル攻撃ができる状態なら
+		if (m_mobMonster->IsSkillUsable() == true)
+		{
+			//スキル攻撃
+			m_mobMonster->SetNextAnimationState(enAnimationState_Skill);
+		}
+		else
+		{
+			//通常攻撃
+			m_mobMonster->SetNextAnimationState(enAnimationState_Attack);
+		}
+	}
+	else
+	{
+		//攻撃できないならその場で待機
+		m_mobMonster->SetNextAnimationState(enAninationState_Idle);
+	}
+}
+
+void MobMonsterSM_Chase::ProcessChase()
+{
+	//攻撃範囲内にプレイヤーがいるなら
+	if (m_mobMonster->IsPlayerInAttackRange() == true)
+	{
+		//攻撃方法を決める
+		ProcessAttackState();
+	}
+	else
+	{
+		//追いかける
+		m_mobMonster->SetNextAnimationState(enAninationState_Chase);
+	}
+
+}
