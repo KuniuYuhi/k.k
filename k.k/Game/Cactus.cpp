ba320660ab2c6df0ea816c6f51a59de4a5f46791
azuki <yuhi55.k@gmail.com>
2024-01-06 02:18:34 +0900

    ノックバック時に回転しないようにした。ノックバック後の硬直するようにした。

--- k.k/Game/Cactus.cpp
+++ k.k/Game/Cactus.cpp
@@ -149,8 +149,13 @@ void Cactus::Update()
 		AngleChangeTimeIntarval(m_angleChangeTime);
 		//毎フレーム行う処理
 		m_mobStateMachine->Execute();
-		//回転処理
-		Rotation(ROT_SPEED, ROT_SPEED);
+
+		//ノックバック中でないなら回転処理
+		if (GetKnockBackFlag() != true)
+		{
+			//回転処理
+			Rotation(ROT_SPEED, ROT_SPEED);
+		}
 
 		//当たり判定
 		DamageCollision(m_charaCon);
@@ -196,21 +201,21 @@ bool Cactus::IsStopProcessing()
 	}
 
 	//ノックバック中なら
-	if (GetKnockBackFlag() == true)
-	{
-		//ノックバックの処理をするなら
-		if (IsProcessKnockBack(
-			m_moveSpeed, m_knockBackTimer) == true)
-		{
-			//座標を移動
-			m_position = m_charaCon.Execute(m_moveSpeed, 1.0f / 60.0f);
-			return true;
-		}
-		else
-		{
-			SetKnockBackFlag(false);
-		}
-	}
+	//if (GetKnockBackFlag() == true)
+	//{
+	//	//ノックバックの処理をするなら
+	//	if (IsKnockingBack(
+	//		m_moveSpeed, m_knockBackTimer) == true)
+	//	{
+	//		//座標を移動
+	//		m_position = m_charaCon.Execute(m_moveSpeed, 1.0f / 60.0f);
+	//		return true;
+	//	}
+	//	else
+	//	{
+	//		SetKnockBackFlag(false);
+	//	}
+	//}
 
 	//それ以外なら
 	return false;
@@ -428,13 +433,27 @@ void Cactus::OnProcessPlantToBattleStateTransition()
 
 void Cactus::OnProcessDamageStateTransition()
 {
-	//アニメーションの再生が終わったら
-	if (m_modelRender.IsPlayingAnimation() == false)
+	if (GetKnockBackFlag() == false)
 	{
-
-		//共通の状態遷移処理に移行
-		ProcessCommonStateTransition();
+		//何フレームか硬直させてから
+		//硬直が終わったら
+		if (IsKnockBackStiffness() == false)
+		{
+			//共通の状態遷移処理に移行
+			ProcessCommonStateTransition();
+		}
+		return;
 	}
+	//ノックバック処理
+	ProcessKnockBack(m_charaCon);
+
+	//アニメーションの再生が終わったら
+	//if (m_modelRender.IsPlayingAnimation() == false)
+	//{
+
+	//	//共通の状態遷移処理に移行
+	//	ProcessCommonStateTransition();
+	//}
 }
 
 
