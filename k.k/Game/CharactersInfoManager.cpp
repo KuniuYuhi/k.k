97f028ff8fd6ef9128e04e117eb427dc48b3fea2
azuki <yuhi55.k@gmail.com>
2024-01-09 01:34:18 +0900

    プレイヤーから一番近いモンスターの検索とカウント

--- k.k/Game/CharactersInfoManager.cpp
+++ k.k/Game/CharactersInfoManager.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "CharactersInfoManager.h"
 
+namespace {
+	const float MONSTER_NEAR_PLAYER_DISTANCE = 350.0f;
+}
 
 CharactersInfoManager* CharactersInfoManager::m_charactersInfoInstance = nullptr;
 
@@ -16,4 +19,57 @@ CharactersInfoManager::CharactersInfoManager()
 CharactersInfoManager::~CharactersInfoManager()
 {
 	m_charactersInfoInstance = nullptr;
+}
+
+void CharactersInfoManager::Execute()
+{
+	SearchMonstersNearPlayer();
+}
+
+void CharactersInfoManager::SearchMonstersNearPlayer()
+{
+	//カウントを0にする
+	m_monstersNearPlayerCount = 0;
+
+	if (m_playerInstance == nullptr)
+	{
+		return;
+	}
+
+	//モンスターの数が0なら
+	if (m_mobMonsters.size() == 0)
+	{
+		return;
+	}
+
+	//
+	//for (auto monster : m_mobMonsters)
+	//{
+	//	//プレイヤーからモンスターに向かうベクトルを計算
+	//	Vector3 diff = monster->GetPosition() - m_playerInstance->GetPosition();
+	//	//距離が一定以下なら
+	//	if (diff.Length() < MONSTER_NEAR_PLAYER_DISTANCE)
+	//	{
+	//		//カウントを加算
+	//		m_monstersNearPlayerCount++;
+	//	}
+	//}
+
+}
+
+bool CharactersInfoManager::SearchMonsterNearPlayer(
+	MobMonster* mobMonster)
+{
+	Vector3 diff= mobMonster->GetPosition() - m_playerInstance->GetPosition();
+
+	//距離が一定以下なら
+	if (diff.Length() < MONSTER_NEAR_PLAYER_DISTANCE)
+	{
+		//カウントを加算
+		m_monstersNearPlayerCount++;
+		//プレイヤーに近い
+		return true;
+	}
+	//プレイヤーに近くない
+	return false;
 }
