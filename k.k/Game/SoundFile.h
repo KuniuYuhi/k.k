93471dc596c160631332fdb7a1b689f93b13478e
azuki <yuhi55.k@gmail.com>
2024-02-02 11:47:00 +0900

    音声ファイルの読み込み

--- k.k/Game/SoundFile.h
+++ k.k/Game/SoundFile.h
@@ -5,6 +5,50 @@ public:
 	SoundFile();
 	 ~SoundFile();
 
-	 void SetSoundFile();
+	 /// <summary>
+	 /// 全てのサウンドファイルの読み込み
+	 /// </summary>
+	 void ResistAllSoundFile();
+
+private:
+
+	/// <summary>
+	/// タイトルのサウンドファイル読み込み
+	/// </summary>
+	void ResistTitleSoundFile();
+	/// <summary>
+	/// バトル開始のサウンドファイル読み込み
+	/// </summary>
+	void ResistBattleStartSoundFile();
+	/// <summary>
+	/// ボス登場のサウンドファイル読み込み
+	/// </summary>
+	void ResistEntryBossSoundFile();
+	/// <summary>
+	/// バトル中のサウンドファイル読み込み
+	/// </summary>
+	void ResistBattleSoundFile();
+	/// <summary>
+	/// プレイヤーのサウンドファイル読み込み
+	/// </summary>
+	void ResistPlayerSoundFile();
+	/// <summary>
+	/// モンスターのサウンドファイル読み込み
+	/// </summary>
+	void ResistMonsterSoundFile();
+	/// <summary>
+	/// ボスのサウンドファイル読み込み
+	/// </summary>
+	void ResistBossSoundFile();
+	/// <summary>
+	/// モブモンスターのサウンドファイル読み込み
+	/// </summary>
+	void ResistMobMonsterSoundFile();
+	/// <summary>
+	/// リザルトのサウンドファイル読み込み
+	/// </summary>
+	void ResistResultSoundFile();
+
+	
 };
 
