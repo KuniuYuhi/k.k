c92a9d4bdd8be352968013bb10d30ed1b6c4b2d9
azuki <yuhi55.k@gmail.com>
2024-02-06 11:34:07 +0900

    文字が中心になるようにした。処理を関数に分けた

--- k.k/Game/DamageFont.cpp
+++ k.k/Game/DamageFont.cpp
@@ -9,9 +9,14 @@ namespace {
 	const float Y_UP_MONSTER = 50.0f;
 	const float Y_UP_BOSS = 170.0f;
 
-	const Vector3 BACK_SCALE_PLAYER = { 1.2f,1.2f,0.0f };
-	const Vector3 BACK_SCALE_MONSTER = { 1.2f,1.2f,0.0f };
-	const Vector3 BACK_SCALE_BOSS = { 1.5f,1.5f,0.0f };
+	const float BACK_SPRITE_SCALE_PLAYER = 1.2f;
+	const float BACK_SPRITE_SCALE_MOBMONSTER = 1.2f;
+	const float BACK_SPRITE_SCALE_BOSS = 1.5f;
+
+	const float DEFAULT_X_OFFSET = -25.0f;
+	const float DEFAULT_Y_OFFSET = 18.0f;
+
+	const int RAMDOM_RANGE = 50.0f;
 }
 
 DamageFont::DamageFont()
@@ -24,10 +29,7 @@ DamageFont::~DamageFont()
 
 bool DamageFont::Start()
 {
-	
 	InitFont();
-
-
 	//todo 与えるダメージの文字サイズによってオフセット
 		
 
@@ -42,113 +44,162 @@ void DamageFont::Update()
 		DeleteGO(this);
 	}
 
-
+	//透明にしていく
 	TransparencyFont();
-
+	//移動処理
 	Move();
-
-
-
 }
 
 void DamageFont::InitFont()
 {
-	m_damageBackRender.Init("Assets/sprite/InGame/DamageEffect/Damage.DDS", 150, 150);
-	
+	//背景の画像の初期化
+	m_damageBackRender.Init(
+		"Assets/sprite/InGame/DamageEffect/Damage.DDS", 150, 150
+	);
 
+	//ダメージフォントのオフセットを計算
+	Vector2 fontOffset = g_vec2Zero;
+
+
+	//ダメージを受けるアクターによって設定
 	switch (m_damageActor)
 	{
 	case DamageFont::enDamageActor_Player:
-		//文字の色を赤にする
-		m_color = g_vec4Red;
-		m_damageFont.SetColor(m_color);
-		m_damageFont.SetScale(SCALE_PLAYER);
-		m_damageFont.SetOffset(24.0f, -12.0f);
-		m_yUp = Y_UP_PLAYER;
-		//画像を拡大
-		m_damageBackRender.SetScale(BACK_SCALE_PLAYER);
+		//ダメージフォントのオフセットを計算
+		fontOffset = CalcDamageFontOffset(SCALE_PLAYER);
+		//ダメージフォントと背景の設定
+		SetDamageSpriteAndFont(
+			g_vec4Red, SCALE_PLAYER, fontOffset,
+			Y_UP_PLAYER, BACK_SPRITE_SCALE_PLAYER
+		);
 		break;
 	case DamageFont::enDamageActor_Monster:
-		//文字の色を白にする
-		m_color = g_vec4White;
-		m_damageFont.SetColor(m_color);
-		m_damageFont.SetScale(SCALE_MONSTER);
-		m_damageFont.SetOffset(24.0f, -12.0f);
-		m_yUp = Y_UP_MONSTER;
-		//画像を拡大
-		m_damageBackRender.SetScale(BACK_SCALE_MONSTER);
+		//ダメージフォントのオフセットを計算
+		fontOffset = CalcDamageFontOffset(SCALE_MONSTER);
+		//ダメージフォントと背景の設定
+		SetDamageSpriteAndFont(
+			g_vec4White, SCALE_MONSTER, fontOffset,
+			Y_UP_MONSTER, BACK_SPRITE_SCALE_MOBMONSTER
+		);
 		break;
 	case DamageFont::enDamageActor_Boss:
-		//文字の色を白にする
-		m_color = g_vec4White;
-		m_damageFont.SetColor(m_color);
-		m_damageFont.SetScale(SCALE_BOSS);
-		m_damageFont.SetOffset(24.0f, -13.0f);
-		m_yUp = Y_UP_BOSS;
-		//画像を拡大
-		m_damageBackRender.SetScale(BACK_SCALE_BOSS);
+		//ダメージフォントのオフセットを計算
+		fontOffset = CalcDamageFontOffset(SCALE_BOSS);
+		//ダメージフォントと背景の設定
+		SetDamageSpriteAndFont(
+			g_vec4White, SCALE_BOSS, fontOffset,
+			Y_UP_MONSTER, BACK_SPRITE_SCALE_BOSS
+		);
 		//座標をランダムにする
-		m_positionRandomFlag = true;
+		m_largeRandomPositionFlag = true;
 		break;
 	default:
 		std::abort();
 		break;
 	}
 
-	Vector3 DamagePosition = m_position;
-	DamagePosition.y += m_yUp;
-	//座標を変換する
-	g_camera3D->CalcScreenPositionFromWorldPosition(m_damagePosition, DamagePosition);
-
-	if (m_positionRandomFlag == true)
-	{
-		//ランダムな座標にする
-		m_damagePosition.x += (rand() % (100 - (-100) + 1)) + (-100);
-		m_damagePosition.y += (rand() % (100 - (-100) + 1)) + (-100);
-	}
-	else
-	{
-		m_damagePosition.x += (rand() % (50 - (-50) + 1)) + (-50);
-		m_damagePosition.y += (rand() % (50 - (-50) + 1)) + (-50);
-	}
+	//ダメージフォントを生成する座標を計算
+	m_damagePosition = CalcDamagePosition();
 	
-
 	wchar_t damage[255];
 	swprintf_s(damage, 255, L"%d", m_damage);
+	//ダメージ量を設定
 	m_damageFont.SetText(damage);
+	//影をつける
 	m_damageFont.SetShadowParam(true, 2.0f, g_vec4Black);
-
+	//座標を設定
 	m_damageFont.SetPosition(m_damagePosition);
 
-	//画像
+	//ダメージフォントの背景の座標を設定
 	m_damageBackPosition.x = m_damagePosition.x;
 	m_damageBackPosition.y = m_damagePosition.y;
-
 	m_damageBackRender.SetPosition(m_damageBackPosition);
+	//更新
 	m_damageBackRender.Update();
 }
 
 void DamageFont::Move()
 {
+	//上昇させる
 	m_damagePosition.y += g_gameTime->GetFrameDeltaTime() * 60.0f;
-
+	//背景のY座標をフォントと合わせる
 	m_damageBackPosition.y = m_damagePosition.y;
-
+	//フォントの座標を設定
 	m_damageFont.SetPosition(m_damagePosition);
-
+	//背景の座標を設定と更新
 	m_damageBackRender.SetPosition(m_damageBackPosition);
 	m_damageBackRender.Update();
 }
 
 void DamageFont::TransparencyFont()
 {
+	//色を透明にしていく
 	m_color.w -= g_gameTime->GetFrameDeltaTime() * 2.2f;
 	m_backColor.w = m_color.w;
-
+	//フォントの色の設定
 	m_damageFont.SetColor(m_color);
-
+	//背景の色の設定と更新
 	m_damageBackRender.SetMulColor(m_backColor);
 	m_damageBackRender.Update();
+}
+
+void DamageFont::SetDamageSpriteAndFont(
+	Vector4 fontColor, 
+	float fontScale, Vector2 fontOffset, float yUp, 
+	float backSpriteScale)
+{
+	//文字の色を設定
+	m_color = fontColor;
+	SetDamageFontInfo(m_color, fontScale, fontOffset);
+	//Y座標を少し上げる
+	m_yUp = yUp;
+	//画像を拡大
+	m_damageBackRender.SetScale(g_vec3One * backSpriteScale);
+}
+
+void DamageFont::SetDamageFontInfo(Vector4 color, float scale, Vector2 offset)
+{
+	m_damageFont.SetColor(color);
+	m_damageFont.SetScale(scale);
+	m_damageFont.SetOffset(offset);
+}
+
+Vector2 DamageFont::CalcDamagePosition()
+{
+	//ダメージを受けるキャラクターの座標のY座標を少し上げる
+	Vector3 DamagePosition = m_position;
+	DamagePosition.y += m_yUp;
+
+	Vector2 finalPos = g_vec2Zero;
+
+	//座標をワールド座標からスクリーン座標に変換
+	g_camera3D->CalcScreenPositionFromWorldPosition(finalPos, DamagePosition);
+	//広い範囲で座標をランダムな場所にしたいなら
+	if (m_largeRandomPositionFlag == true)
+	{
+		finalPos.x += (rand() % (RAMDOM_RANGE*2 - (-RAMDOM_RANGE*2) + 1)) + (-RAMDOM_RANGE*2);
+		finalPos.y += (rand() % (RAMDOM_RANGE*2 - (-RAMDOM_RANGE*2) + 1)) + (-RAMDOM_RANGE*2);
+	}
+	else
+	{
+		finalPos.x += (rand() % (RAMDOM_RANGE - (-RAMDOM_RANGE) + 1)) + (-RAMDOM_RANGE);
+		finalPos.y += (rand() % (RAMDOM_RANGE - (-RAMDOM_RANGE) + 1)) + (-RAMDOM_RANGE);
+	}
+
+	return finalPos;
+}
+
+Vector2 DamageFont::CalcDamageFontOffset(float fontScale)
+{
+	//ダメージの桁数を取得する
+	int digitCount = static_cast<int>(log10(m_damage) + 1);
+	Vector2 finalOffset = g_vec2Zero;
+	//Yオフセット
+	finalOffset.y = DEFAULT_X_OFFSET * fontScale;
+	//Xオフセット
+	finalOffset.x = digitCount * DEFAULT_Y_OFFSET * fontScale;
+
+	return finalOffset;
 }
 
 void DamageFont::Render(RenderContext& rc)
--- k.k/Game/DamageFont.h
+++ k.k/Game/DamageFont.h
@@ -13,24 +13,13 @@ public:
 	{
 		enDamageActor_Player,
 		enDamageActor_Monster,
-		enDamageActor_Boss
+		enDamageActor_Boss,
+		enDamageActor_None
 	};
 
-	bool Start();
-	void Update();
-	void Render(RenderContext& rc);
-
-	void InitFont();
-
-	/// <summary>
-	/// フォントの移動処理
-	/// </summary>
-	void Move();
-
-	/// <summary>
-	/// フォントの透明化
-	/// </summary>
-	void TransparencyFont();
+	bool Start() override;
+	void Update() override;
+	void Render(RenderContext& rc) override;
 
 	/// <summary>
 	/// 生成時に必要な設定
@@ -44,27 +33,93 @@ public:
 		SetPosition(position);
 	}
 
+	
+
+private:
+
+	/// <summary>
+	/// 表示するフォントの初期化
+	/// </summary>
+	void InitFont();
+
+	/// <summary>
+	/// フォントの移動処理
+	/// </summary>
+	void Move();
+
+	/// <summary>
+	/// フォントの透明化
+	/// </summary>
+	void TransparencyFont();
+
+	/// <summary>
+	/// ダメージの設定
+	/// </summary>
+	/// <param name="damage"></param>
 	void SetDamage(int damage)
 	{
 		m_damage = damage;
 	}
-
+	/// <summary>
+	/// ダメージを受けるアクターを設定
+	/// </summary>
+	/// <param name="damageActor"></param>
 	void SetDamageActor(EnDamageActor damageActor)
 	{
 		m_damageActor = damageActor;
 	}
-
+	/// <summary>
+	/// 生成する座標を設定
+	/// </summary>
+	/// <param name="position"></param>
 	void SetPosition(Vector3 position)
 	{
 		m_position = position;
 	}
+
+	/// <summary>
+	/// ダメージフォントと背景の設定
+	/// </summary>
+	/// <param name="fontColor">フォントのカラー</param>
+	/// <param name="fontScale">フォントの拡大率</param>
+	/// <param name="fontOffset">フォントのオフセット</param>
+	/// <param name="yUp">Yアップ</param>
+	/// <param name="backSpriteScale">背景の拡大率</param>
+	void SetDamageSpriteAndFont(
+		Vector4 fontColor,
+		float fontScale,
+		Vector2 fontOffset,
+		float yUp,
+		float backSpriteScale
+	);
+
+	/// <summary>
+	/// ダメージフォントの情報を設定
+	/// </summary>
+	/// <param name="color">色</param>
+	/// <param name="scale">拡大率</param>
+	/// <param name="offset">オフセット</param>
+	void SetDamageFontInfo(Vector4 color, float scale, Vector2 offset);
+
+	/// <summary>
+	/// ダメージフォントを表示する座標を設定
+	/// </summary>
+	/// <returns></returns>
+	Vector2 CalcDamagePosition();
+
+	/// <summary>
+	/// ダメージフォントのオフセットを計算
+	/// </summary>
+	/// <param name="fontScale">フォントのスケール</param>
+	/// <returns></returns>
+	Vector2 CalcDamageFontOffset(float fontScale = 1.0f);
 
 private:
 
 	FontRender m_damageFont;
 	SpriteRender m_damageBackRender;
 
-	EnDamageActor m_damageActor;
+	EnDamageActor m_damageActor = enDamageActor_None;
 
 	Vector3 m_position = g_vec3Zero;
 	Vector2 m_damagePosition = g_vec2Zero;
@@ -75,12 +130,12 @@ private:
 	Vector4 m_color = g_vec4White;
 	Vector4 m_backColor = g_vec4White;
 
-	int m_damage;
-	float m_yUp;
+	int m_damage = 0;
+	float m_yUp = 0.0f;
 
 	float m_Alpha = 1.0f;
 
-	bool m_positionRandomFlag = false;
+	bool m_largeRandomPositionFlag = false;
 
 };
 
