5251369543c6850c2d0dc9c4a3f5472cb24b7777
azuki <yuhi55.k@gmail.com>
2024-02-14 14:29:53 +0900

    コメント追加。当たり判定の変数名変更

--- k.k/Game/ComboFinishBomb.h
+++ k.k/Game/ComboFinishBomb.h
@@ -10,11 +10,18 @@ public:
 	bool Start() override;
 	void Update() override;
 	
+	/// <summary>
+	/// 座標を設定
+	/// </summary>
+	/// <param name="position"></param>
 	void SetPosition(Vector3 position)
 	{
 		m_position = position;
 	}
-
+	/// <summary>
+	/// 座標に行列を適応
+	/// </summary>
+	/// <param name="matrix"></param>
 	void ApplyPositionToMatrix(Matrix matrix)
 	{
 		matrix.Apply(m_position);
@@ -23,16 +30,20 @@ public:
 
 
 private:
-
+	/// <summary>
+	/// エフェクト再生
+	/// </summary>
 	void PlayBombEffect();
-
+	/// <summary>
+	/// 当たり判定生成
+	/// </summary>
 	void CreatCollision();
 
 private:
 
 	Vector3 m_position = g_vec3Zero;
 
-
+	CollisionObject* m_collision = nullptr;
 
 };
 
