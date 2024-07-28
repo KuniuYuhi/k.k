b4c10823c1679fa5845a891d166c2a160cfec219
azuki <yuhi55.k@gmail.com>
2024-07-29 00:32:03 +0900

    前方向を追加

--- k.k/Game/ICharacter.h
+++ k.k/Game/ICharacter.h
@@ -14,18 +14,24 @@ public:
 		return m_position;
 	}
 
+	const Vector3& GetForward() const
+	{
+		return m_forward;
+	}
+
 	const Vector3& GetScale() const
 	{
 		return m_scale;
 	}
 
-	const Vector3& GetPosition() const
+	const Quaternion& GetRotation() const
 	{
-		return m_position;
+		return m_rotation;
 	}
 
 protected:
 	Vector3 m_position = Vector3::Zero;
+	Vector3 m_forward = Vector3::Front;
 	Vector3 m_scale = Vector3::One;
 	Quaternion m_rotation = Quaternion::Identity;
 
