2e52f0f0b3110e032dd2f7df5e016dd492d1571f
azuki <yuhi55.k@gmail.com>
2024-07-31 11:52:15 +0900

    モブエネミー継承用クラス作成

---
+++ k.k/Game/MobEnemyBase.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "EnemyBase.h"
+
+
+
+/// <summary>
+/// モブエネミー用のベースクラス
+/// モブエネミーで共通する処理を実装する
+/// </summary>
+class MobEnemyBase : public EnemyBase
+{
+public:
+
+	virtual ~MobEnemyBase() override = default;
+
+
+	/// <summary>
+	/// 更新処理。使ってない！！！！
+	/// </summary>
+	virtual void UpdateCharacter() override {}
+
+protected:
+
+
+
+
+protected:
+
+
+
+};
+
