53a42c2525515892ddc0f1658d7be6a7227ff825
azuki <yuhi55.k@gmail.com>
2024-07-31 11:30:16 +0900

    コンポーネントを作成時の最初の処理を行う関数追加

--- k.k/k2EngineLow/IComponent.h
+++ k.k/k2EngineLow/IComponent.h
@@ -7,6 +7,11 @@ namespace nsK2EngineLow {
 		virtual ~IComponent() = default;
 
 		/// <summary>
+		/// コンポーネントを追加した時の最初の処理
+		/// </summary>
+		virtual void Start(){}
+
+		/// <summary>
 		/// 更新処理。自動で行ってくれる。消したいならGameObjectManagerへ
 		/// </summary>
 		virtual void UpdateComponent() {}
