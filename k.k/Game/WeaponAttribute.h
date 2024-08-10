fad36fd885b235eebf97bde7419c838147c93a25
azuki <yuhi55.k@gmail.com>
2024-08-11 00:12:34 +0900

    武器属性変更記載ヘッダー作成

---
+++ k.k/Game/WeaponAttribute.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+namespace WeaponType {
+
+	/// <summary>
+	/// 武器の属性
+	/// </summary>
+	enum EnWeaponAttribute
+	{
+		enWeaponAttribute_Slash,	//斬タイプ
+		enWeaponAttribute_Pierce,	//突タイプ
+	};
+
+}
