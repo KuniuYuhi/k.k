8197e3ac796b1dcb8c71cdb777ad49fa1097c68d
azuki <yuhi55.k@gmail.com>
2024-08-13 01:39:27 +0900

    武器の情報を記載したヘッダー作成

---
+++ k.k/Game/WeaponInfo.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/// <summary>
+/// 武器の属性のネームスペース
+/// </summary>
+namespace WeaponAttribute {
+
+	/// <summary>
+	/// 武器の属性
+	/// </summary>
+	enum EnWeaponAttribute
+	{
+		enWeaponAttribute_Slash,	//斬タイプ
+		enWeaponAttribute_Pierce,	//突タイプ
+	};
+
+}
+
+/// <summary>
+/// 武器の種類のネームスペース
+/// </summary>
+namespace WeaponType {
+
+	/// <summary>
+	/// 武器の種類
+	/// </summary>
+	enum EnWeaponType
+	{
+		enSwordShield,			//ソード＆シールド
+		enGreateSword,			//グレイトソード
+		enBowArrow				//ボウ＆アロー
+	};
+
+}
