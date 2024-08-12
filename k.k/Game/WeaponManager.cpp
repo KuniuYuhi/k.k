11e2a048eb88b3fe4ddeb6f1c928e4d7a6c12681
azuki <yuhi55.k@gmail.com>
2024-08-13 01:39:49 +0900

    武器切り替え処理追加

--- k.k/Game/WeaponManager.cpp
+++ k.k/Game/WeaponManager.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "WeaponManager.h"
 
+#include "SwordShield.h"
+#include "GreateSword.h"
+#include "Bow.h"
+
+
+
 WeaponManager* WeaponManager::m_instance = nullptr;
 
 WeaponManager::WeaponManager()
@@ -20,6 +26,85 @@ WeaponManager::~WeaponManager()
 void WeaponManager::CreateInstance()
 {
 	m_instance = new WeaponManager();
+}
+
+void WeaponManager::Init(
+	EnWeaponType main, EnWeaponType sub, EnWeaponType sub2)
+{
+	//武器をマップに格納する
+	m_weaponsMap.insert(std::make_pair(enSwordShield, NewGO<SwordShield>(0, "SwordShield")));
+	m_weaponsMap.insert(std::make_pair(enGreateSword, NewGO<GreateSword>(0, "GreateSword")));
+	m_weaponsMap.insert(std::make_pair(enBowArrow, NewGO<Bow>(0, "Bow")));
+
+	//全ての武器の状態を収納状態にする
+	for (auto& weapon : m_weaponsMap)
+	{
+		weapon.second->ChangeStowedState();
+	}
+	
+	//武器の役割を設定する
+	SettingUseWeaponRole(main, sub, sub2);
+
+	//メイン武器のキーに対応する要素を取得。現在の武器とする
+	m_armedWeapon = m_weaponsMap.at(m_mainWeaponType);
+	//武器を装備状態にする
+	m_armedWeapon->SetCurrentWeaponState(WeaponBase::enArmed);
+
+}
+
+void WeaponManager::SettingUseWeaponRole(
+	EnWeaponType main, EnWeaponType sub, EnWeaponType sub2)
+{
+	m_mainWeaponType = main;
+	m_subWeaponType = sub;
+	m_subWeaponType2 = sub2;
+}
+
+void WeaponManager::ChangeArmedWeapon(EnWeaponType armedWeaponType)
+{
+	//現在装備している武器を収納状態にする
+	m_armedWeapon->ChangeStowedState();
+
+	//装備する武器をマップから持ってくる
+	m_armedWeapon = m_weaponsMap.at(armedWeaponType);
+	//マップになければ
+	if (m_armedWeapon == nullptr)
+	{
+		std::abort();
+	}
+	//武器を装備状態にする
+	m_armedWeapon->ChangeArmedState();
+
+	//メイン武器の中身を切り替え対象武器に切り替える
+	ChangeChangeWeaponTypeToMainWeaponType();
+}
+
+void WeaponManager::SetChangeWeaponType(EnWeaponType targetWeaponType)
+{
+	m_changeTargetWeaponType = targetWeaponType;
+}
+
+void WeaponManager::ChangeSubWeaponTypeToChangeWeaponType()
+{
+	m_changeTargetWeaponType = m_subWeaponType;
+	//一時的に切り替え対象武器のアドレスを保存
+	m_tempWeaponType = &m_subWeaponType;
+}
+
+void WeaponManager::ChangeSubWeaponType2ToChangeWeaponType()
+{
+	m_changeTargetWeaponType = m_subWeaponType2;
+	//一時的に切り替え対象武器のアドレスを保存
+	m_tempWeaponType = &m_subWeaponType2;
+}
+
+void WeaponManager::ChangeChangeWeaponTypeToMainWeaponType()
+{
+	//保存しておいた入れ替え元の変数をメイン武器のものに変更
+	*m_tempWeaponType = m_mainWeaponType;
+	//メイン武器の中身を切り替え武器のものに変更
+	m_mainWeaponType = m_changeTargetWeaponType;
+
 }
 
 
--- k.k/Game/WeaponManager.h
+++ k.k/Game/WeaponManager.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "WeaponInfo.h"
+
+class WeaponBase;
+
+
+using namespace WeaponType;
+
 /// <summary>
 /// 武器を管理するマネージャークラス。
 /// シングルトンパターン
@@ -37,10 +44,100 @@ public:
 		m_instance = nullptr;
 	}
 
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	/// <param name="armedWeaponType">最初に装備する武器のタイプ</param>
+	void Init(EnWeaponType main, EnWeaponType sub, EnWeaponType sub2);
+
+	/// <summary>
+	/// 使う武器の役割を設定する
+	/// </summary>
+	/// <param name="main">メイン武器のタイプ</param>
+	/// <param name="sub">サブ武器１のタイプ</param>
+	/// <param name="sub2">サブ武器２にタイプ</param>
+	void SettingUseWeaponRole(EnWeaponType main, EnWeaponType sub, EnWeaponType sub2);
+
+	/// <summary>
+	/// 装備する武器を切り替える
+	/// </summary>
+	/// <param name="armedWeaponType">切り替える武器のタイプ</param>
+	void ChangeArmedWeapon(EnWeaponType armedWeaponType);
+
+
+
+	/// <summary>
+	/// 切り替え武器タイプを設定
+	/// </summary>
+	/// <param name="targetWeaponType">切り替え武器タイプ</param>
+	void SetChangeWeaponType(EnWeaponType targetWeaponType);
+
+	/// <summary>
+	/// サブ武器１を切り替え武器にする
+	/// </summary>
+	void ChangeSubWeaponTypeToChangeWeaponType();
+
+	/// <summary>
+	/// サブ武器２を切り替え武器にする
+	/// </summary>
+	void ChangeSubWeaponType2ToChangeWeaponType();
+
+	/// <summary>
+	/// 切り替え武器をメイン武器にする
+	/// </summary>
+	void ChangeChangeWeaponTypeToMainWeaponType();
+
+	/// <summary>
+	/// 装備している武器を取得
+	/// </summary>
+	/// <returns></returns>
+	WeaponBase* GetArmedWeapon()
+	{
+		return m_armedWeapon;
+	}
+
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	EnWeaponType GetMainWeaponType() const
+	{
+		return m_mainWeaponType;
+	}
+
+	/// <summary>
+	/// 切り替え武器タイプを取得
+	/// </summary>
+	/// <returns></returns>
+	EnWeaponType GetChangeTargetWeaponType() const
+	{
+		return m_changeTargetWeaponType;
+	}
+
 
 private:
 
+	
 
+
+
+	
+
+
+private:
+
+	std::map<EnWeaponType, WeaponBase*> m_weaponsMap;		//武器を格納しておくマップ
+
+	WeaponBase* m_armedWeapon = nullptr;
+
+	EnWeaponType m_mainWeaponType;		//メイン武器タイプ
+	EnWeaponType m_subWeaponType;		//サブ武器1
+	EnWeaponType m_subWeaponType2;		//サブ武器2
+	EnWeaponType m_changeTargetWeaponType;	//切り替え対象の武器タイプ
+
+	EnWeaponType* m_tempWeaponType;
 
 };
 
