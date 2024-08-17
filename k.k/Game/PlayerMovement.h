90cab30ba7ed92532f00bc3d2b02fc93113f8696
azuki <yuhi55.k@gmail.com>
2024-08-18 01:58:00 +0900

    プレイヤーの移動処理の方法を追加

--- k.k/Game/PlayerMovement.h
+++ k.k/Game/PlayerMovement.h
@@ -21,32 +21,48 @@ public:
 	void UpdateComponent() override;
 
 
-	int GetSpeed()
-	{
-		return m_speed;
-	}
+	
 
 	/// <summary>
-	/// 普段の移動速度の
-	/// 計算
+	/// 普段の移動速度の計算。プレイヤーの前方向も設定するよ！
 	/// </summary>
 	/// <param name="playerStatus">ステータス</param>
 	/// <param name="moveSpeed">現在の移動速度</param>
 	/// <param name="StickInput">コントローラーの移動スティックの入力量</param>
 	/// <returns>移動速度</returns>
 	Vector3 CalcSimpleMovementVerocity(
-		Status_Player playerStatus,
+		float speed,
 		Vector3 moveSpeed,
 		Vector3 StickInput
 	);
 
+
+	
+	/// <summary>
+	/// 移動方向のみの計算
+	/// </summary>
+	/// <param name="forward">前方向</param>
+	/// <param name="moveSpeed">現在の移動速度</param>
+	/// <returns></returns>
+	Vector3 CalcForwardDirection(Vector3 forward, Vector3 moveSpeed = g_vec3Zero);
+
+
+	/// <summary>
+	/// 移動方向のみの計算。入力方向も考慮
+	/// </summary>
+	/// <param name="forward">前方向</param>
+	/// <param name="stick">入力方向</param>
+	/// <param name="moveSpeed">現在の移動速度</param>
+	/// <returns></returns>
+	Vector3 CalcMoveDirection(Vector3 forward, Vector3 stick, Vector3 moveSpeed = g_vec3Zero);
 
 
 private:
 
 	Brave* m_brave = nullptr;
 
-	int m_speed = 10;
+
+	Vector3 m_moveDirection = g_vec3Zero;		//移動方向のみ。正規化すること！！
 
 
 };
